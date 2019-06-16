ee9d2e8a8197cf629e43c53fe7e60664245b29e3
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-06-16 14:29:54 +0200

    add colors and coin rename

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -2,37 +2,41 @@
 
 set -e
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+LTGREEN='\033]1;32m'
+
 tx () {
-    echo ">$ sscli tx swapservice $@"
+    tput setaf 2; echo ">$ sscli tx swapservice $@"; tput sgr0
     echo "password" | sscli tx swapservice $@ --from jack --yes --broadcast-mode block
 }
 
 query () {
-    echo ">$ sscli query swapservice $@"
+    tput setaf 2; echo ">$ sscli query swapservice $@"; tput sgr0
     sscli query swapservice $@
 }
 
 # Setup wallets
 tx set-account alice ATOM 100000
-tx set-account alice BTC 38
+tx set-account alice CANYA 38
 tx set-account jack ATOM 100000
-tx set-account jack BTC 104
-tx set-account jack COIN2 1008
+tx set-account jack CANYA 104
+tx set-account jack MARVEL 1008
 
 query accstruct alice
 query accstruct jack
 
 # Stake Coins
-tx set-stake alice BTC 58 12
-tx set-stake alice COIN2 578 308
-tx set-stake jack BTC 55 88
-tx set-stake jack COIN2 600 97
+tx set-stake alice CANYA 58 12
+tx set-stake alice MARVEL 578 308
+tx set-stake jack CANYA 55 88
+tx set-stake jack MARVEL 600 97
 
-query stakestruct BTC
-query stakestruct COIN2
+query stakestruct CANYA
+query stakestruct MARVEL
 
 # Swap Coins
 query accstruct alice
-tx set-swap ATOM BTC 10 alice alice
-tx set-swap BTC COIN2 10 alice alice
+tx set-swap ATOM CANYA 10 alice alice
+tx set-swap CANYA MARVEL 10 alice alice
 query accstruct alice
