2b7b2ac11784e492d1256c7fb8db4dea36250e69
Johnny Luo <johnnyl@campaignmonitor.com>
2019-07-25 08:17:47 -0700

    add some help functions for types

--- x/swapservice/types/staker_pool.go
+++ x/swapservice/types/staker_pool.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"fmt"
+	"strings"
+)
+
 // StakePool represent staker and their activities in the pools
 // {
 //	"id":"bnbStakerAddr1",
@@ -11,4 +16,24 @@ package types
 type StakerPool struct {
 	StakerID  string               `json:"id"` // this will be staker's address on binance chain
 	PoolUnits map[string][3]string `json:"pu"` // the key of this map will be the pool id , value will bt [UNIT,RUNE,TOKEN]
+}
+
+// NewStakerPool create a new instance of StakerPool
+func NewStakerPool(id string) StakerPool {
+	return StakerPool{
+		StakerID:  id,
+		PoolUnits: make(map[string][3]string),
+	}
+}
+
+// String return a user readable string representation of Staker Pool
+func (sp StakerPool) String() string {
+	sb := strings.Builder{}
+	sb.WriteString(fmt.Sprintln("staker-id: " + sp.StakerID))
+	if nil != sp.PoolUnits {
+		for key, item := range sp.PoolUnits {
+			sb.WriteString(fmt.Sprintf("%s - %s ", key, strings.Join(item[:], ",")))
+		}
+	}
+	return sb.String()
 }
