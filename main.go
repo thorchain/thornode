a08902ef7d967d7ab238ae006af00cef0a18933e
Brent McDowell <brent@bmcd.co>
2019-07-30 13:55:36 +0800

    Lightweight HTTP implementation.

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"os"
 	"time"
 	silverback "gitlab.com/thorchain/bepswap/observe/x/silverback"
+	rest "gitlab.com/thorchain/bepswap/observe/x/rest"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	rest.Start(port)
+	
 	poolAddress := os.Getenv("POOL_ADDRESS")
 	dexHost := os.Getenv("DEX_HOST")
 	silverback.Start(30 * time.Second, poolAddress, dexHost)
