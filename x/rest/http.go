101f439182028d4519a96f2bf4ebe54dc2702782
Buri <buri@thorchain.org>
2019-07-30 13:55:36 +0800

    Lightweight HTTP implementation.

---
+++ x/rest/http.go
@@ -0,0 +1,26 @@
+package rest
+
+import (
+	"net/http"
+	"encoding/json"
+
+	types "gitlab.com/thorchain/bepswap/observe/x/rest/types"
+)
+
+func Start(port string) {
+  http.HandleFunc("/", Status)
+  http.ListenAndServe(":" + port, nil)
+}
+
+func Status(w http.ResponseWriter, r *http.Request) {
+	data := types.Response{Status: "OK"}
+
+  js, err := json.Marshal(data)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(js)
+}
---
+++ x/rest/types/response.go
@@ -0,0 +1,5 @@
+package types
+
+type Response struct {
+  Status string
+}
