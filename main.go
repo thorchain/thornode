b616607be6cce0d4dceca588ba6cb8cb87fef82c
Brent McDowell <brent@bmcd.co>
2019-08-01 22:08:18 +0800

    Support TXN -> RUNE swaps.

--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import "gitlab.com/thorchain/bepswap/observe/x/silverback"
 
 func main() {
 	binance := silverback.NewBinance()
+	pool := silverback.NewPool(binance.PoolAddress)
 	
 	silverback.SyncBal(*binance)
-	silverback.NewServer(*binance).Start()
-	silverback.NewClient(*binance).Start()
+	silverback.NewServer(*binance, *pool).Start()
+	silverback.NewClient(*binance, *pool).Start()
 }
