0f5f37a032d5bf56f34ad611c66a0243bb5b25ea
Buri <buri@thorchain.org>
2019-08-01 22:11:59 +0800

    Cleanup.

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "gitlab.com/thorchain/bepswap/observe/x/silverback"
 func main() {
 	binance := silverback.NewBinance()
 	pool := silverback.NewPool(binance.PoolAddress)
-	
+
 	silverback.SyncBal(*binance)
 	silverback.NewServer(*binance, *pool).Start()
 	silverback.NewClient(*binance, *pool).Start()
