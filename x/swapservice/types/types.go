b19eb1360a7e41316837d6b8da7e4a7b83e2b84a
Chad Barraford <cbarraford@gmail.com>
2019-08-02 15:47:46 +0200

    add types

--- x/swapservice/types/types.go
+++ x/swapservice/types/types.go
@@ -1,1 +1,41 @@
 package types
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Pool - metadata about a pool
+type Pool struct {
+	Address     string `json:"address"`      // unique BNB address to store staked tokens
+	TokenName   string `json:"token_name"`   // display name of token (ie "Bitcoin")
+	TokenTicker string `json:"token_ticker"` // ticker name of token (ie "BTC")
+}
+
+func NewPool(name, ticker string) Pool {
+	// TODO add address
+	return Pool{
+		TokenName:   name,
+		TokenTicker: strings.ToUpper(ticker),
+	}
+}
+
+func (p Pool) Key() string {
+	return p.TokenTicker
+}
+
+func (p Pool) String() string {
+	return fmt.Sprintf("Pool: %s (%s) %s", p.TokenName, p.TokenTicker, p.Address)
+}
+
+type StakeTx struct {
+	TxHash string `json:"tx_hash"` // binance chain tx hash of coins sent to our address
+}
+
+func (tx StakeTx) Key() string {
+	return tx.TxHash
+}
+
+func (tx StakeTx) String() string {
+	return fmt.Sprintf("TxHash: %s", tx.TxHash)
+}
