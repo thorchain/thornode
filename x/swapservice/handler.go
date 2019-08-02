d4992563d909956dc4a1cf7b7f190a47fca8b366
Chad Barraford <cbarraford@gmail.com>
2019-08-02 17:10:19 +0200

    add address when creating a pool

--- x/swapservice/handler.go
+++ x/swapservice/handler.go
@@ -3,6 +3,10 @@ package swapservice
 import (
 	"fmt"
 
+	exchange "github.com/jpthor/cosmos-swap/exchange"
+	storage "github.com/jpthor/cosmos-swap/storage"
+	"github.com/rs/zerolog/log"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -23,6 +27,34 @@ func handleMsgSetPool(ctx sdk.Context, keeper Keeper, msg MsgSetPool) sdk.Result
 	// validate there are not conflicts first
 	if keeper.PoolDoesExist(ctx, msg.Pool.Key()) {
 		return sdk.ErrUnknownRequest("Conflict").Result()
+	}
+
+	/////////////////////////////////////////////////////////////////////
+	// TODO: this is hacky, should not implement wallet services within the
+	// handler
+	/////////////////////////////////////////////////////////////////////
+	dir := "~/.ssd/wallets"
+	ds, err := storage.NewDataStore(dir, log.Logger)
+	if nil != err {
+		return sdk.ErrUnknownRequest(err.Error()).Result()
+	}
+	ws, err := exchange.NewWallets(ds, log.Logger)
+	if err != nil {
+		return sdk.ErrUnknownRequest(err.Error()).Result()
+	}
+
+	wallet, err := ws.GetWallet(msg.Pool.TokenTicker)
+	if err != nil {
+		return sdk.ErrUnknownRequest(err.Error()).Result()
+	}
+	msg.Pool.Address, err = sdk.AccAddressFromHex(wallet.PublicAddress)
+	if err != nil {
+		return sdk.ErrUnknownRequest(err.Error()).Result()
+	}
+	////////////////////////////////////////////////////////////////////
+
+	if msg.Pool.Empty() {
+		return sdk.ErrUnknownRequest("Invalid Pool").Result()
 	}
 
 	keeper.SetPool(ctx, msg.Pool)
