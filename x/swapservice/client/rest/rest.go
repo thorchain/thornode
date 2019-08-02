f6ba9e7dd0367d4d89ecc28ac07a672806fe379c
Chad Barraford <cbarraford@gmail.com>
2019-08-02 17:28:14 +0200

    add REST API endpoints

--- x/swapservice/client/rest/rest.go
+++ x/swapservice/client/rest/rest.go
@@ -5,61 +5,134 @@ import (
 	"net/http"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/rest"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
 
 	"github.com/gorilla/mux"
+
+	types "github.com/jpthor/cosmos-swap/x/swapservice/types"
 )
 
 const (
-	stakeData = "stakedata"
-	accData   = "accdata"
-	swapData  = "swapdata"
+	poolID = "poolID"
 )
 
-// TODO add the new features to Restful routes
-// pool staker , staker pool etc
-// pool index etc
 // RegisterRoutes - Central function to define routes that get registered by the main application
-func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, storePoolData string) {
-	r.HandleFunc(fmt.Sprintf("/%s/pools", storePoolData), poolHandler(cliCtx, storePoolData)).Methods("GET")
-	r.HandleFunc(fmt.Sprintf("/%s/account/{%s}", storePoolData, accData), accHandler(cliCtx, storePoolData)).Methods("GET")
-	r.HandleFunc(fmt.Sprintf("/%s/stake/{%s}", storePoolData, stakeData), stakeHandler(cliCtx, storePoolData)).Methods("GET")
-	r.HandleFunc(fmt.Sprintf("/%s/swaprecord/{%s}", storePoolData, swapData), stakeHandler(cliCtx, storePoolData)).Methods("GET")
+func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, storeName string) {
+	r.HandleFunc(fmt.Sprintf("/%s/stake/tx", storeName), stakeHandler(cliCtx)).Methods("POST")
+	r.HandleFunc(fmt.Sprintf("/%s/pool", storeName), createPoolHandler(cliCtx)).Methods("POST")
+
+	r.HandleFunc(fmt.Sprintf("/%s/pools", storeName), listPoolsHandler(cliCtx, storeName)).Methods("GET")
+	r.HandleFunc(fmt.Sprintf("/%s/pool/{%s}", storeName, poolID), getPoolHandler(cliCtx, poolID)).Methods("GET")
+
 }
 
-func poolHandler(cliCtx context.CLIContext, storePoolData string) http.HandlerFunc {
+// ---------------------------------------------------------------------------------
+// Tx Handler
+
+type stakeReq struct {
+	BaseReq rest.BaseReq `json:"base_req"`
+	TxHash  string       `json:"tx_hash"`
+}
+
+func stakeHandler(cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/pooldatas", storePoolData), nil)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+		var req stakeReq
+
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
 			return
 		}
-		rest.PostProcessResponse(w, cliCtx, res)
+
+		baseReq := req.BaseReq.Sanitize()
+		if !baseReq.ValidateBasic(w) {
+			return
+		}
+
+		addr, err := sdk.AccAddressFromBech32(req.BaseReq.From)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		// create the message
+		msg := types.NewMsgSetTxHash(req.TxHash, addr)
+		err = msg.ValidateBasic()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, baseReq, []sdk.Msg{msg})
 	}
 }
 
-func accHandler(cliCtx context.CLIContext, storePoolData string) http.HandlerFunc {
+type createPoolReq struct {
+	BaseReq     rest.BaseReq `json:"base_req"`
+	TokenName   string       `json:"token_name"`
+	TokenTicker string       `json:"token_ticker"`
+}
+
+func createPoolHandler(cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		paramType := vars[accData]
-		res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/accstruct/%s", storePoolData, paramType), nil)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+		var req createPoolReq
+
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
 			return
 		}
-		rest.PostProcessResponse(w, cliCtx, res)
+
+		baseReq := req.BaseReq.Sanitize()
+		if !baseReq.ValidateBasic(w) {
+			return
+		}
+
+		addr, err := sdk.AccAddressFromBech32(req.BaseReq.From)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		// create the message
+		msg := types.NewMsgSetPool(req.TokenName, req.TokenTicker, addr)
+		err = msg.ValidateBasic()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, baseReq, []sdk.Msg{msg})
 	}
 }
 
-func stakeHandler(cliCtx context.CLIContext, storePoolData string) http.HandlerFunc {
+//--------------------------------------------------------------------------------
+// Query Handlers
+
+func listPoolsHandler(cliCtx context.CLIContext, storeCmName string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		paramType := vars[stakeData]
-		res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/stakestruct/%s", storePoolData, paramType), nil)
+		res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/pools", storeCmName), nil)
 		if err != nil {
 			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
 			return
 		}
+
+		rest.PostProcessResponse(w, cliCtx, res)
+
+	}
+}
+
+func getPoolHandler(cliCtx context.CLIContext, storeName string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		paramType := vars[poolID]
+
+		res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/pool/%s", storeName, paramType), nil)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+			return
+		}
+
 		rest.PostProcessResponse(w, cliCtx, res)
 	}
 }
