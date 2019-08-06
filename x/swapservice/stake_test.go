89730a68d362c657d5e7c54822de23be6758c976
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-08-06 10:27:15 +0200

    pr feedback

--- x/swapservice/stake_test.go
+++ x/swapservice/stake_test.go
@@ -1,6 +1,8 @@
 package swapservice
 
 import (
+	"fmt"
+
 	"github.com/pkg/errors"
 	. "gopkg.in/check.v1"
 )
@@ -88,26 +90,27 @@ func (s StakeSuite) TestCalculatePoolUnits(c *C) {
 }
 
 func (s StakeSuite) TestValidateAmount(c *C) {
-	makePoolStaker := func(total int, avg string) PoolStaker {
+	makePoolStaker := func(total int, avg float64) PoolStaker {
 		stakers := make([]StakerUnit, total)
 		for i, _ := range stakers {
-			stakers[i] = StakerUnit{Units: avg}
+			stakers[i] = StakerUnit{Units: fmt.Sprintf("%f", avg)}
 		}
 
 		return PoolStaker{
-			Stakers: stakers,
+			TotalUnits: fmt.Sprintf("%f", avg*float64(total)),
+			Stakers:    stakers,
 		}
 	}
 
-	skrs := makePoolStaker(50, "0.001")
+	skrs := makePoolStaker(50, 0.001)
 	c.Assert(validateStakeAmount(skrs, 0.001), IsNil)
 
-	skrs = makePoolStaker(150, "0.0002")
+	skrs = makePoolStaker(150, 0.0002)
 	c.Assert(validateStakeAmount(skrs, 0.0001), NotNil)
 	c.Assert(validateStakeAmount(skrs, 0.0002), NotNil)
 	c.Assert(validateStakeAmount(skrs, 0.0010), IsNil)
 
-	skrs = makePoolStaker(300, "0.001")
+	skrs = makePoolStaker(300, 0.001)
 	c.Assert(validateStakeAmount(skrs, 0.0001), NotNil)
 	c.Assert(validateStakeAmount(skrs, 0.002), NotNil)
 	c.Assert(validateStakeAmount(skrs, 0.004), IsNil)
