9705dd7e949719cf8d69efb9f4dce8185525a91f
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-08-07 13:14:15 +0200

    post tx

--- tests/integration/spec/api_spec.rb
+++ tests/integration/spec/api_spec.rb
@@ -12,9 +12,16 @@ describe "API Tests" do
   context "Check that an empty tx hash returns properly" do
     it "should have no values" do
       resp = get("/tx/bogus")
-      puts resp.body
       expect(resp.body['request']).to eq ""
       expect(resp.body['status']).to eq ""
+      expect(resp.body['txhash']).to eq ""
+    end
+  end
+
+  context "Create a pool" do
+    it "create a pool for bnb" do
+      resp = processTx("FE0EC391E62AF2E291A41C38DEB3DF180FB0FD0E21E8B3866EFF8912F65FD1EE")
+      puts resp.body
       expect(resp.body['txhash']).to eq ""
     end
   end
