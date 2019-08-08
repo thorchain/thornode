494c415dadd07065be756bd58b3bb8268dc5e1d6
Chad Barraford <cbarraford@gmail.com>
2019-08-08 10:59:49 +0200

    squash to a single docker container

--- tests/integration/wait-for-blockchain.sh
+++ tests/integration/wait-for-blockchain.sh
@@ -8,18 +8,18 @@ echo "Waiting for Blockchain..."
 
 cmd="$@"
 
-until curl -s "$DAEMONHOST:$DAEMONPORT"; do
+until curl -s "localhost:26657"; do
   >&2 echo "Blockchain is unavailable - sleeping"
   sleep 1
 done
 
-until curl -s "$APIHOST:$APIPORT/swapservice/ping"; do
+until curl -s "localhost:1317/swapservice/ping"; do
   >&2 echo "Rest server is unavailable - sleeping"
   sleep 1
 done
 
 # sleep a little more to give time to add its first block
-sleep 10
+sleep 3
 
 >&2 echo "Blockchain is up - executing command"
 exec $cmd
