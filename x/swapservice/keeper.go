1ba36556ce2de26381298aab6ed3671aaabd776f
Johnny Luo <johnnyluo1980@gmail.com>
2019-08-12 07:53:24 +0000

    remove coin keeper from Keeper, because we don't use it at all

--- x/swapservice/keeper.go
+++ x/swapservice/keeper.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/bank"
 	"github.com/pkg/errors"
 	"github.com/tendermint/tendermint/libs/log"
 )
@@ -34,17 +33,15 @@ func getKey(prefix dbPrefix, key string) string {
 
 // Keeper maintains the link to data storage and exposes getter/setter methods for the various parts of the state machine
 type Keeper struct {
-	coinKeeper bank.Keeper
-	storeKey   sdk.StoreKey // Unexposed key to access store from sdk.Context
-	cdc        *codec.Codec // The wire codec for binary encoding/decoding.
+	storeKey sdk.StoreKey // Unexposed key to access store from sdk.Context
+	cdc      *codec.Codec // The wire codec for binary encoding/decoding.
 }
 
 // NewKeeper creates new instances of the swapservice Keeper
-func NewKeeper(coinKeeper bank.Keeper, storeKey sdk.StoreKey, cdc *codec.Codec) Keeper {
+func NewKeeper(storeKey sdk.StoreKey, cdc *codec.Codec) Keeper {
 	return Keeper{
-		coinKeeper: coinKeeper,
-		storeKey:   storeKey,
-		cdc:        cdc,
+		storeKey: storeKey,
+		cdc:      cdc,
 	}
 }
 
