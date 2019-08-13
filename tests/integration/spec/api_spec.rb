b4230945ffa22efe29bc79ff7843ff7fbf8f96ee
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-08-13 11:45:22 +0200

    tweaks

--- tests/integration/spec/api_spec.rb
+++ tests/integration/spec/api_spec.rb
@@ -5,6 +5,7 @@ describe "API Tests" do
   context "Check /ping responds" do
     it "should return 'pong'" do
       resp = get("/ping")
+      expect(resp.code).to eq("200")
       expect(resp.body['ping']).to eq "pong"
     end
   end
@@ -19,21 +20,31 @@ describe "API Tests" do
   end
 
   context "Create a pool" do
+
+    it "should show up in listing of pools" do
+      resp = get("/pools")
+      # should have one pool added via genesis
+      expect(resp.body.length).to eq(1), resp.body
+    end
+
     it "create a pool for bnb" do
       resp = processTx("create:TCAN-014")
       expect(resp.code).to eq("200"), "Are you working from a clean blockchain? \n(#{resp.code}: #{resp.body})"
     end
 
-    it "should get a conflict the second time" do
-      # resp = processTx("create:TCAN-014")
-      # expect(resp.code).to eq("500")
-    end
-
-    sleep(3) # give time for the block 
-
     it "should be able to get the pool" do
       resp = get("/pool/TCAN-014")
       expect(resp.body['ticker']).to eq("TCAN-014"), resp.body
+    end
+
+
+    it "should not create a duplicate pool" do
+      resp = processTx("create:TCAN-014")
+      expect(resp.code).to eq("500")
+      
+      resp = get("/pools")
+      # should have one pool added via genesis
+      expect(resp.body.length).to eq(2), resp.body
     end
 
     it "should show up in listing of pools" do
