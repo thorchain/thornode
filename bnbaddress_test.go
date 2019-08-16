88625e9203673a397e8c9848dffd7c1a58136516
Chad Barraford <cbarraford@gmail.com>
2019-08-16 10:43:18 +0200

    breakout types.go

---
+++ bnbaddress_test.go
@@ -0,0 +1,29 @@
+package types
+
+import (
+	. "gopkg.in/check.v1"
+)
+
+type BnBAddressSuite struct{}
+
+var _ = Suite(&BnBAddressSuite{})
+
+func (s *BnBAddressSuite) TestBnbAddress(c *C) {
+	addr, err := NewBnbAddress("bnbblejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6")
+	c.Check(err, IsNil)
+	c.Check(addr.IsEmpty(), Equals, false)
+	c.Check(addr.Equals(BnbAddress("bnbblejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6")), Equals, true)
+	_, err = NewBnbAddress("bnb1lejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6")
+	c.Check(err, IsNil)
+	_, err = NewBnbAddress("tbnb1lejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6")
+	c.Check(err, IsNil)
+	_, err = NewBnbAddress("1lejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6")
+	c.Check(err, NotNil)
+	_, err = NewBnbAddress("bnb1lejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6X")
+	c.Check(err, NotNil)
+	_, err = NewBnbAddress("bogus")
+	c.Check(err, NotNil)
+	c.Check(BnbAddress("").IsEmpty(), Equals, true)
+
+	c.Check(NoBnbAddress.Equals(BnbAddress("")), Equals, true)
+}
