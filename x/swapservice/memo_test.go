10325c6490c0096b7dab5f62d1e4b1e7c92161f0
Chad Barraford <cbarraford@gmail.com>
2019-09-11 14:25:06 +0200

    fixes #74

--- x/swapservice/memo_test.go
+++ x/swapservice/memo_test.go
@@ -18,7 +18,7 @@ func (s *MemoSuite) TestTxType(c *C) {
 }
 func (s *MemoSuite) TestParseWithAbbreviated(c *C) {
 	// happy paths
-	memo, err := ParseMemo("&:RUNE-1BA")
+	memo, err := ParseMemo("c:RUNE-1BA")
 	c.Assert(err, IsNil)
 	c.Check(memo.GetTicker().String(), Equals, "RUNE-1BA")
 	c.Check(memo.IsType(txCreate), Equals, true, Commentf("MEMO: %+v", memo))
@@ -39,7 +39,7 @@ func (s *MemoSuite) TestParseWithAbbreviated(c *C) {
 	c.Check(memo.IsType(txWithdraw), Equals, true, Commentf("MEMO: %+v", memo))
 	c.Check(memo.GetAmount(), Equals, "25")
 
-	memo, err = ParseMemo(">:RUNE-1BA:bnb1lejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6:8.7")
+	memo, err = ParseMemo("=:RUNE-1BA:bnb1lejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6:8.7")
 	c.Assert(err, IsNil)
 	c.Check(memo.GetTicker().String(), Equals, "RUNE-1BA")
 	c.Check(memo.IsType(txSwap), Equals, true, Commentf("MEMO: %+v", memo))
@@ -47,14 +47,14 @@ func (s *MemoSuite) TestParseWithAbbreviated(c *C) {
 	c.Log(memo.GetSlipLimit().Uint64())
 	c.Check(memo.GetSlipLimit().Equal(sdk.NewUint(870000000)), Equals, true)
 
-	memo, err = ParseMemo(">:RUNE-1BA:bnb1lejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6")
+	memo, err = ParseMemo("=:RUNE-1BA:bnb1lejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6")
 	c.Assert(err, IsNil)
 	c.Check(memo.GetTicker().String(), Equals, "RUNE-1BA")
 	c.Check(memo.IsType(txSwap), Equals, true, Commentf("MEMO: %+v", memo))
 	c.Check(memo.GetDestination().String(), Equals, "bnb1lejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6")
 	c.Check(memo.GetSlipLimit().Uint64(), Equals, uint64(0))
 
-	memo, err = ParseMemo(">:RUNE-1BA:bnb1lejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6:")
+	memo, err = ParseMemo("=:RUNE-1BA:bnb1lejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6:")
 	c.Assert(err, IsNil)
 	c.Check(memo.GetTicker().String(), Equals, "RUNE-1BA")
 	c.Check(memo.IsType(txSwap), Equals, true, Commentf("MEMO: %+v", memo))
@@ -80,13 +80,13 @@ func (s *MemoSuite) TestParseWithAbbreviated(c *C) {
 	c.Assert(err, NotNil)
 	_, err = ParseMemo("CREATE") // missing symbol
 	c.Assert(err, NotNil)
-	_, err = ParseMemo("&:") // bad symbol
+	_, err = ParseMemo("c:") // bad symbol
 	c.Assert(err, NotNil)
 	_, err = ParseMemo("-:bnb") // withdraw basis points is optional
 	c.Assert(err, IsNil)
 	_, err = ParseMemo("-:bnb:twenty-two") // bad amount
 	c.Assert(err, NotNil)
-	_, err = ParseMemo(">:bnb:bad_DES:5.6") // bad destination
+	_, err = ParseMemo("=:bnb:bad_DES:5.6") // bad destination
 	c.Assert(err, NotNil)
 	_, err = ParseMemo(">:bnb:bnb1lejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6:five") // bad slip limit
 	c.Assert(err, NotNil)
