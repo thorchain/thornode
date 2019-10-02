192ecead777fda7b514ef254dd70bb567d8fd4a3
Brent McDowell <brent@bmcd.co>
2019-10-03 09:15:42 +1300

    Removed init().

--- x/smoke/statechain.go
+++ x/smoke/statechain.go
@@ -2,24 +2,24 @@ package smoke
 
 import "fmt"
 
-var endpoints map[string]string
+var endpoints = map[string]string{
+	"stage": "testnet-chain.bepswap.io",
+	"dev":   "testnet-chain.bepswap.net",
+	"prod":  "testnet-chain.bepswap.com",
+}
 
-type Statechain struct{
+type Statechain struct {
 	Env string
 }
 
-func init() {
-	endpoints["stage"] = "testnet-chain.bepswap.io"
-	endpoints["dev"] = "testnet-chain.bepswap.net"
-	endpoints["prod"] = "testnet-chain.bepswap.com"
-}
-
+// NewStatechain : Create a new Statechain instance.
 func NewStatechain(env string) Statechain {
 	return Statechain{
 		Env: env,
 	}
 }
 
+// PoolURL : Return the Pool URL based on the selected environment.
 func (s Statechain) PoolURL() string {
 	return fmt.Sprintf("https://%v/swapservice/pools", endpoints[s.Env])
 }
