7c69bb4f1ddb0f456d80439bc38c9715ccae6253
Brent McDowell <brent@bmcd.co>
2019-10-03 08:56:04 +1300

    Removed hardcoded references to the Binance chainID.

--- x/smoke/types/binance.go
+++ x/smoke/types/binance.go
@@ -1,8 +1,3 @@
 package types
 
-const (
-	Scheme         = "https"
-	ChainId        = "Binance-Chain-Nile"
-	BroadcastTxURI = "api/v1/broadcast"
-	Multiplier     = 100000000
-)
+const Multiplier = 100000000
