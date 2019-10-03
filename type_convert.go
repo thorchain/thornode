b6907c48571f207186253006bf82e03aeadeda96
Loki <l031@protonmail.com>
2019-10-03 17:12:42 +1000

    Added helper functions

--- type_convert.go
+++ type_convert.go
@@ -14,6 +14,21 @@ func UintToFloat64(input sdk.Uint) float64 {
 	return float64(input.Uint64())
 }
 
+// UintToUint64
+func UintToUint64(input sdk.Uint) uint64 {
+	return input.Uint64()
+}
+
+//
+func IntToInt64(input sdk.Int) int64 {
+	return input.Int64()
+}
+
+func IntToUint64(input sdk.Int) uint64 {
+	return uint64(input.Int64())
+}
+
+
 func FloatToUintAndMultipleOne(input float64) sdk.Uint {
 	return sdk.NewUint(uint64(math.Round(input * One)))
 }
