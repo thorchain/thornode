c3f13e5bc9db1c9e50568ab049d7cf75e83b7d15
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-10-15 18:00:49 +1100

    drop trust account prefix

--- x/swapservice/versioning.go
+++ x/swapservice/versioning.go
@@ -2,13 +2,13 @@ package swapservice
 
 func getVersion(height int64, prefix dbPrefix) int {
 	switch prefix {
-	case prefixTrustAccount:
-		return getTrustAccountVersion(height)
+	case prefixNodeAccount:
+		return getNodeAccountVersion(height)
 	default:
 		return 0
 	}
 }
 
-func getTrustAccountVersion(height int64) int {
+func getNodeAccountVersion(height int64) int {
 	return 0
 }
