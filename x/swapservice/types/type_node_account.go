f99ed12d6a9d3955c5854ba8d7722fd5ef0fe2a1
Johnny <johnnyluo1980@gmail.com>
2019-10-15 00:09:21 +0000

    Add some unit tests , and also fix a refund bug

--- x/swapservice/types/type_node_account.go
+++ x/swapservice/types/type_node_account.go
@@ -8,8 +8,6 @@ import (
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/tendermint/tendermint/crypto"
-	tmtypes "github.com/tendermint/tendermint/types"
 	"gitlab.com/thorchain/bepswap/common"
 )
 
@@ -141,17 +139,6 @@ func (n NodeAccount) String() string {
 	sb.WriteString("account:" + n.Accounts.String() + "\n")
 	sb.WriteString("bond:" + n.Bond.String() + "\n")
 	return sb.String()
-}
-
-// GetRandomNodeAccount create a random generated node account , used for test purpose
-func GetRandomNodeAccount(status NodeStatus) NodeAccount {
-	name := RandStringBytesMask(10)
-	addr := sdk.AccAddress(crypto.AddressHash([]byte(name)))
-	bnb, _ := common.NewBnbAddress("tbnb" + RandStringBytesMask(39))
-	v, _ := tmtypes.RandValidator(true, 100)
-	k, _ := sdk.Bech32ifyConsPub(v.PubKey)
-	na := NewNodeAccount(addr, status, NewTrustAccount(bnb, addr, k))
-	return na
 }
 
 // NodeAccounts just a list of NodeAccount
