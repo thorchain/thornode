993bf0304b15d8b857c343502eaba66864b8cd94
Chad Barraford <cbarraford@gmail.com>
2019-10-16 13:29:19 +1100

    tweak

--- x/swapservice/versioning.go
+++ x/swapservice/versioning.go
@@ -10,11 +10,5 @@ func getVersion(version int, prefix dbPrefix) int {
 }
 
 func getNodeAccountVersion(version int) int {
-	// if the lowest installed version is 2 or greater, use version 1 of node
-	// accoutns
-	if version >= 2 {
-		return 1
-	}
-
 	return 0 // default
 }
