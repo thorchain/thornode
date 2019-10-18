b057455681b2e53391443b05ef1fc5b7455f91f3
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-10-18 19:58:57 +1100

    rate limiting

--- x/swapservice/client/rest/rest.go
+++ x/swapservice/client/rest/rest.go
@@ -2,8 +2,11 @@ package rest
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
 	"github.com/gorilla/mux"
 	"gitlab.com/thorchain/bepswap/statechain/x/swapservice/query"
 )
@@ -17,19 +20,29 @@ const (
 // RegisterRoutes - Central function to define routes that get registered by the main application
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, storeName string) {
 
+	// limit api calls
+	lmt := tollbooth.NewLimiter(10, &limiter.ExpirableOptions{DefaultExpirationTTL: time.Hour})
+	lmt.SetMessage("You have reached maximum request limit.")
+
 	// Health Check Endpoint
-	r.HandleFunc(
+	r.Handle(
 		fmt.Sprintf("/%s/ping", storeName),
-		pingHandler(cliCtx, storeName),
+		tollbooth.LimitFuncHandler(
+			lmt,
+			pingHandler(cliCtx, storeName),
+		),
 	).Methods("GET")
 
 	// Dynamically create endpoints of all funcs in querier.go
 	for _, q := range query.Queries {
 		endpoint := q.Endpoint(storeName, restURLParam, restURLParam2)
 		if endpoint != "" { // don't setup REST endpoint if we have no endpoint
-			r.HandleFunc(
+			r.Handle(
 				endpoint,
-				getHandlerWrapper(q, storeName, cliCtx),
+				tollbooth.LimitFuncHandler(
+					lmt,
+					getHandlerWrapper(q, storeName, cliCtx),
+				),
 			).Methods("GET")
 		}
 	}
