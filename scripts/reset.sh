e436ed8bc8847e4a662c1121bd5ab2428db7ff47
Chad Barraford <cbarraford@gmail.com>
2019-10-29 18:03:10 +1100

    fix reset script

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -18,18 +18,20 @@ while true; do
   sscli config indent true
   sscli config trust-node true
 
+  if [ -z "${NET:-}" ]; then
+    echo "ProdNetwork"
+  fi
   if [ -z "${POOL_ADDRESS:-}" ]; then
     echo "empty pool address"
     POOL_ADDRESS=bnb1lejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6
   fi
   # add jack as a trusted account
   {
-    jq --arg VERSION "$(sscli query swapservice version | jq -r .version)" --arg POOL_ADDRESS "$POOL_ADDRESS" --arg VALIDATOR "$(ssd tendermint show-validator)" --arg NODE_ADDRESS "$(sscli keys show jack -a)" --arg OBSERVER_ADDRESS "$(sscli keys show jack -a)" '.app_state.swapservice.node_accounts[0] = {"node_address": $NODE_ADDRESS, "version": $VERSION, "status":"active","bond_address":$POOL_ADDRESS,"accounts":{"bnb_signer_acc": $POOL_ADDRESS, "bepv_validator_acc": $VALIDATOR, "bep_observer_acc": $OBSERVER_ADDRESS}}'
+    jq --arg VERSION "$(sscli query swapservice version | jq -r .version)" --arg POOL_ADDRESS "$POOL_ADDRESS" --arg VALIDATOR "$(ssd tendermint show-validator)" --arg NODE_ADDRESS "$(sscli keys show jack -a)" --arg OBSERVER_ADDRESS "$(sscli keys show jack -a)" '.app_state.swapservice.node_accounts[0] = {"node_address": $NODE_ADDRESS, "version": $VERSION, "status":"active","bond_address":$POOL_ADDRESS,"accounts":{"bnb_signer_acc": $POOL_ADDRESS, "bepv_validator_acc": $VALIDATOR, "bep_observer_acc": $OBSERVER_ADDRESS}} | .app_state.swapservice.pool_addresses.rotate_at="28800" | .app_state.swapservice.pool_addresses.rotate_window_open_at="27800" | .app_state.swapservice.pool_addresses.current = $POOL_ADDRESS'
   } <~/.ssd/config/genesis.json >/tmp/genesis.json
   mv /tmp/genesis.json ~/.ssd/config/genesis.json
-
+  cat ~/.ssd/config/genesis.json
   ssd validate-genesis
-
   break
 
 done
