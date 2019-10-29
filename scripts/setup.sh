fca74a006f407feca0be4204a37c42dec10ac184
Johnny Luo <johnnyluo1980@gmail.com>
2019-10-29 14:58:00 +1100

    issue164 try to debug integration test

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -27,9 +27,8 @@ while true; do
     jq --arg VERSION "$(sscli query swapservice version | jq -r .version)" --arg POOL_ADDRESS "$POOL_ADDRESS" --arg VALIDATOR "$(ssd tendermint show-validator)" --arg NODE_ADDRESS "$(sscli keys show jack -a)" --arg OBSERVER_ADDRESS "$(sscli keys show jack -a)" '.app_state.swapservice.node_accounts[0] = {"node_address": $NODE_ADDRESS, "version": $VERSION, "status":"active","bond_address":$POOL_ADDRESS,"accounts":{"bnb_signer_acc": $POOL_ADDRESS, "bepv_validator_acc": $VALIDATOR, "bep_observer_acc": $OBSERVER_ADDRESS}} | .app_state.swapservice.pool_addresses.rotate_at="28800" | .app_state.swapservice.pool_addresses.rotate_window_open_at="27800" | .app_state.swapservice.pool_addresses.current = $POOL_ADDRESS'
   } <~/.ssd/config/genesis.json >/tmp/genesis.json
   mv /tmp/genesis.json ~/.ssd/config/genesis.json
-
-  ssd validate-genesis
   cat ~/.ssd/config/genesis.json
+  ssd validate-genesis
   break
 
 done
