6628cba738cd54f42318c63e3bd0ddd82d927c9b
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-11-05 16:55:37 +1100

    tweaks

--- build/scripts/core.sh
+++ build/scripts/core.sh
@@ -5,7 +5,7 @@ add_node_account () {
     OBSERVER_ADDRESS=$3
     VERSION=$4
     POOL_ADDRESS=$5
-    jq --arg VERSION "$VERSION" --arg POOL_ADDRESS "$POOL_ADDRESS" --arg VALIDATOR "$VALIDATOR" --arg NODE_ADDRESS "$NODE_ADDRESS" --arg OBSERVER_ADDRESS "$OBSERVER_ADDRESS" '.app_state.swapservice.node_accounts[0] = {"node_address": $NODE_ADDRESS, "version": $VERSION, "status":"active","bond_address":$POOL_ADDRESS,"accounts":{"bnb_signer_acc": $POOL_ADDRESS, "bepv_validator_acc": $VALIDATOR, "bep_observer_acc": $OBSERVER_ADDRESS}}' <~/.thord/config/genesis.json >/tmp/genesis.json
+    jq --arg VERSION "$VERSION" --arg POOL_ADDRESS "$POOL_ADDRESS" --arg VALIDATOR "$VALIDATOR" --arg NODE_ADDRESS "$NODE_ADDRESS" --arg OBSERVER_ADDRESS "$OBSERVER_ADDRESS" '.app_state.swapservice.node_accounts += [{"node_address": $NODE_ADDRESS, "version": $VERSION, "status":"active","bond_address":$POOL_ADDRESS,"accounts":{"bnb_signer_acc": $POOL_ADDRESS, "bepv_validator_acc": $VALIDATOR, "bep_observer_acc": $OBSERVER_ADDRESS}}]' <~/.thord/config/genesis.json >/tmp/genesis.json
     mv /tmp/genesis.json ~/.thord/config/genesis.json
 }
 
@@ -13,8 +13,12 @@ add_node_account () {
 add_pool_address () {
     POOL_ADDRESS=$1
     SEQNO=$2
-    jq --arg SEQNO "$SEQNO" --arg POOL_ADDRESS "$POOL_ADDRESS" '.app_state.swapservice.pool_addresses.rotate_at="28800" | .app_state.swapservice.pool_addresses.rotate_window_open_at="27800" | .app_state.swapservice.pool_addresses.current[0] = {"chain":"BNB","seq_no":$SEQNO,"pub_key":$POOL_ADDRESS}' <~/.thord/config/genesis.json >/tmp/genesis.json
+    jq --arg SEQNO "$SEQNO" --arg POOL_ADDRESS "$POOL_ADDRESS" '.app_state.swapservice.pool_addresses.rotate_at="28800" | .app_state.swapservice.pool_addresses.rotate_window_open_at="27800" | .app_state.swapservice.pool_addresses.current += [{"chain":"BNB","seq_no":$SEQNO,"pub_key":$POOL_ADDRESS}]' <~/.thord/config/genesis.json >/tmp/genesis.json
     mv /tmp/genesis.json ~/.thord/config/genesis.json
+}
+
+add_admin_config () {
+    jq --arg NODE_ADDRESS "$3" --arg KEY "$1" --arg VALUE "$2" '.app_state.swapservice.admin_configs += [{"address": $NODE_ADDRESS ,"key":$KEY, "value":$VALUE}]' ~/.thord/config/genesis.json > /tmp/genesis.json
 }
 
 # inits a statechain with a comman separate list of usernames
@@ -24,7 +28,7 @@ init_chain () {
     thord init local --chain-id statechain
 
     for user in $1; do # iterate over our list of comma separated users "alice,jack"
-        echo "password" | thorcli keys add $user
+        echo "$2" | thorcli keys add $user
         thord add-genesis-account $(thorcli keys show $user -a) 1000thor
     done
 
@@ -32,4 +36,53 @@ init_chain () {
     thorcli config output json
     thorcli config indent true
     thorcli config trust-node true
+}
+
+peer_list () {
+    PEER="$1@$2:26656"
+    ADDR='addr_book_strict = true'
+    ADDR_STRICT_FALSE='addr_book_strict = false'
+    PEERSISTENT_PEER_TARGET='persistent_peers = ""'
+
+    sed -i -e "s/$ADDR/$ADDR_STRICT_FALSE/g" ~/.thord/config/config.toml
+    sed -i -e "s/$PEERSISTENT_PEER_TARGET/persistent_peers = \"$PEER\"/g" ~/.thord/config/config.toml
+}
+
+gen_bnb_address () {
+    if [ ! -f ~/.signer/private_key.txt ]; then
+        echo "GENERATING BNB ADDRESSES"
+        # because the generate command can get API rate limited, we may need to retry
+        n=0
+        until [ $n -ge 60 ]; do
+            generate > /tmp/bnb && break
+            n=$[$n+1]
+            sleep 1
+        done
+        ADDRESS=$(cat /tmp/bnb | grep MASTER= | awk -F= '{print $NF}')
+        echo $ADDRESS > ~/.signer/address.txt
+        BINANCE_PRIVATE_KEY=$(cat /tmp/bnb | grep MASTER_KEY= | awk -F= '{print $NF}')
+        echo $BINANCE_PRIVATE_KEY > ~/.signer/private_key.txt
+        PUBKEY=$(cat /tmp/bnb | grep MASTER_PUBKEY= | awk -F= '{print $NF}')
+        echo $PUBKEY > ~/.signer/pubkey.txt
+    fi
+}
+
+fetch_genesis () {
+    until curl -s "$1:26657" > /dev/null; do
+        sleep 3
+    done
+    curl -s $1:26657/genesis | jq .result.genesis > ~/.thord/config/genesis.json
+    thord validate-genesis
+    cat ~/.thord/config/genesis.json
+}
+
+fetch_node_id () {
+    until curl -s "$1:26657" > /dev/null; do
+        sleep 3
+    done
+    curl -s $1:26657/status | jq -r .result.node_info.id
+}
+
+fetch_version () {
+    thorcli query swapservice version --chain-id statechain --trust-node --output json | jq -r .version
 }
--- build/scripts/validator.sh
+++ build/scripts/validator.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -ex
 
+source $(dirname "$0")/core.sh
+
 PEER="${PEER:=none}" # the hostname of a seed node
 SIGNER_NAME="${SIGNER_NAME:=statechain}"
 SIGNER_PASSWD="${SIGNER_PASSWD:=password}"
@@ -11,51 +13,17 @@ if [ ! -f ~/.thord/config/genesis.json ]; then
         exit 1
     fi
 
-    if [ -f ~/.signer/private_key.txt ]; then
-        ADDRESS=$(cat ~/.signer/address.txt)
-    else
-        echo "GENERATING BNB ADDRESSES"
-        # because the generate command can get API rate limited, we may need to retry
-        n=0
-        until [ $n -ge 60 ]; do
-            generate > /tmp/bnb && break
-            n=$[$n+1]
-            sleep 1
-        done
-        ADDRESS=$(cat /tmp/bnb | grep MASTER= | awk -F= '{print $NF}')
-        echo $ADDRESS > ~/.signer/address.txt
-        BINANCE_PRIVATE_KEY=$(cat /tmp/bnb | grep MASTER_KEY= | awk -F= '{print $NF}')
-        echo $BINANCE_PRIVATE_KEY > ~/.signer/private_key.txt
-        PUBKEY=$(cat /tmp/bnb | grep MASTER_PUBKEY= | awk -F= '{print $NF}')
-        echo $PUBKEY > ~/.signer/pubkey.txt
-    fi
+    gen_bnb_address
 
-    # create statechain user
-    echo $SIGNER_PASSWD | thorcli keys add $SIGNER_NAME
+    init_chain $SIGNER_NAME $SIGNER_PASSWD
 
-    NODE_ADDRESS=$(thorcli keys show statechain -a)
+    fetch_genesis $PEER
+
+    NODE_ID=$(fetch_node_id $PEER)
+    peer_list $NODE_ID $PEER
+
+    NODE_ADDRESS=$(thorcli keys show $SIGNER_NAME -a)
     echo "YOUR NODE ADDRESS: $NODE_ADDRESS. Send your bond with this as your address."
-
-    # Setup SSD
-    thord init local --chain-id statechain
-    thorcli config chain-id statechain
-    thorcli config output json
-    thorcli config indent true
-    thorcli config trust-node true
-
-    curl $PEER:26657/genesis | jq .result.genesis > ~/.thord/config/genesis.json
-
-    thord validate-genesis
-
-    NODE_ID=$(curl $PEER:26657/status | jq -r .result.node_info.id)
-    PEER="$NODE_ID@$PEER:26656"
-    ADDR='addr_book_strict = true'
-    ADDR_STRICT_FALSE='addr_book_strict = false'
-    PEERSISTENT_PEER_TARGET='persistent_peers = ""'
-
-    sed -i -e "s/$ADDR/$ADDR_STRICT_FALSE/g" ~/.thord/config/config.toml
-    sed -i -e "s/$PEERSISTENT_PEER_TARGET/persistent_peers = \"$PEER\"/g" ~/.thord/config/config.toml
-
 fi
 
 exec "$@"
