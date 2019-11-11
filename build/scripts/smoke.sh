13ea8e6a849fd7d57eca8eeaa233a6d2a74cacc2
Brent McDowell <brent@bmcd.co>
2019-11-11 16:31:20 +0800

    Minor fix/tweak.

--- build/scripts/smoke.sh
+++ build/scripts/smoke.sh
@@ -28,8 +28,8 @@ run_services() {
   export NODES=1
   export SEED="$(hostname)"
 
-  unset $CHAIN_HOST
-  unset $RPC_HOST
+  unset CHAIN_HOST
+  unset RPC_HOST
 
   $(dirname "$0")/genesis.sh
   run_thord
