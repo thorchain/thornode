f2350d9f695d931d4e4ea5294ef595a7958eac43
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-11-12 14:42:29 +1100

    txIn supports multiple out hashes

--- x/thorchain/types/type_tx_in.go
+++ x/thorchain/types/type_tx_in.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"strings"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/pkg/errors"
 
@@ -8,7 +10,7 @@ import (
 )
 
 type status string
-type TxInIndex []common.TxID
+type TxInIndex common.TxIDs
 
 const (
 	Incomplete status = "incomplete"
@@ -19,9 +21,10 @@ const (
 // Meant to track if we have processed a specific tx
 type TxIn struct {
 	Status             status           `json:"status"`
-	Done               common.TxID      `json:"txhash"` // completed chain tx hash. This is a slice to track if we've "double spent" an input
-	Memo               string           `json:"memo"`   // memo
-	Coins              common.Coins     `json:"coins"`  // coins sent in tx
+	NumOuts            int64            `json:"num_outs"`
+	OutHashes          common.TxIDs     `json:"out_hashes"` // completed chain tx hash. This is a slice to track if we've "double spent" an input
+	Memo               string           `json:"memo"`       // memo
+	Coins              common.Coins     `json:"coins"`      // coins sent in tx
 	Sender             common.Address   `json:"sender"`
 	To                 common.Address   `json:"to"` // to address
 	BlockHeight        sdk.Uint         `json:"block_height"`
@@ -102,7 +105,11 @@ func (tx TxIn) Equals(tx2 TxIn) bool {
 }
 
 func (tx TxIn) String() string {
-	return tx.Done.String()
+	hashes := make([]string, len(tx.OutHashes))
+	for i, h := range tx.OutHashes {
+		hashes[i] = h.String()
+	}
+	return strings.Join(hashes, ", ")
 }
 
 func (tx *TxIn) Sign(signer sdk.AccAddress) {
@@ -114,14 +121,24 @@ func (tx *TxIn) Sign(signer sdk.AccAddress) {
 	tx.Signers = append(tx.Signers, signer)
 }
 
+func (tx *TxIn) SetOutHash(s status, hash common.TxID) {
+	for _, done := range tx.OutHashes {
+		if done.Equals(hash) {
+			return
+		}
+	}
+	tx.OutHashes = append(tx.OutHashes, hash)
+	if int64(len(tx.OutHashes)) >= tx.NumOuts {
+		tx.Status = s
+	}
+}
+
 func (tx *TxIn) SetDone(hash common.TxID) {
-	tx.Status = Done
-	tx.Done = hash
+	tx.SetOutHash(Done, hash)
 }
 
 func (tx *TxIn) SetReverted(hash common.TxID) {
-	tx.Status = Reverted
-	tx.Done = hash
+	tx.SetOutHash(Reverted, hash)
 }
 
 func (tx *TxIn) GetCommonTx(txid common.TxID) common.Tx {
