b699114e67a732232995461f4b4c4a496eb0f7d8
Chad Barraford <cbarraford@gmail.com>
2019-11-13 20:58:05 +1100

    add emissionCurve const

--- x/thorchain/vault_data.go
+++ x/thorchain/vault_data.go
@@ -7,6 +7,14 @@ import (
 	"gitlab.com/thorchain/bepswap/thornode/cmd"
 )
 
+// With feedback from the community, the block reward emission curve has been
+// modified to target 2% emission after 10 years (similiar to Bitcoin).
+// This reduces the return in the first year, but allows it to be spread
+// forward into the future. Since RUNE is a strictly-scarce fixed supply asset,
+// emissions need to be carefully considered for greatest network prosperity.
+// Day 0 Emission is now 25%, Year 1 Emission is 20%.
+const emissionCurve = 5
+
 // calculate node rewards
 func calcNodeRewards(naBlocks, totalUnits, totalRuneReward sdk.Uint) sdk.Uint {
 	reward := sdk.NewUint(uint64(
@@ -32,7 +40,7 @@ func calcPoolRewards(totalPoolRewards, totalStakedRune sdk.Uint, pools []Pool) [
 func calcBlockRewards(totalReserve sdk.Uint) (sdk.Uint, sdk.Uint) {
 	blocksPerYear := 31536000 / cmd.SecondsPerBlock
 	blockRewards := sdk.NewUint(uint64(
-		(float64(totalReserve.Uint64()) / float64(5)) / float64(blocksPerYear),
+		(float64(totalReserve.Uint64()) / float64(emissionCurve)) / float64(blocksPerYear),
 	))
 	poolReward := blockRewards.QuoUint64(3)
 	bondReward := blockRewards.Sub(poolReward)
