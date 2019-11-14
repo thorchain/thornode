8961d6463c07fb15fbd3e7300eaa51e7f3e5fed9
Chad Barraford <cbarraford@gmail.com>
2019-11-15 10:06:40 +1100

    PR feedback

--- common/gas.go
+++ common/gas.go
@@ -1,12 +1,25 @@
 package common
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
 
 type Gas Coins
 
 // Gas Fees
 var BNBGasFeeSingleton = Gas{
 	{Asset: BNBAsset, Amount: sdk.NewUint(37500)},
+}
+
+var BNBGasFeeMulti = Gas{
+	{Asset: BNBAsset, Amount: sdk.NewUint(30000)},
+}
+
+// Calculates the amount of gas for x number of coins in a single tx.
+func GetBNBGasFeeMulti(count uint64) Gas {
+	gas := BNBGasFeeMulti
+	gas[0].Amount = gas[0].Amount.MulUint64(count)
+	return gas
 }
 
 func (g Gas) IsValid() error {
