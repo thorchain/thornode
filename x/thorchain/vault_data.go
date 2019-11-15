4a1f5e0fec19d196a1f12f9bc938a5b50bf27fa5
jpthor <jpthor@pm.me>
2019-11-15 13:53:01 +1100

    emissionCurve and rewards

--- x/thorchain/vault_data.go
+++ x/thorchain/vault_data.go
@@ -8,13 +8,12 @@ import (
 	"gitlab.com/thorchain/bepswap/thornode/cmd"
 )
 
-// With feedback from the community, the block reward emission curve has been
-// modified to target 2% emission after 10 years (similar to Bitcoin).
-// This reduces the return in the first year, but allows it to be spread
-// forward into the future. Since RUNE is a strictly-scarce fixed supply asset,
-// emissions need to be carefully considered for greatest network prosperity.
-// Day 0 Emission is now 25%, Year 1 Emission is 20%.
-const emissionCurve = 5
+// The block reward emission curve targets a ~2% emission after 10 years (similar to Bitcoin).
+// Since RUNE is a strictly-scarce asset, emissions need to be carefully considered for greatest network prosperity.
+// Day 0 Emission is ~25%, Year 1 Emission is ~20%.
+
+const emissionCurve = 6         // An arbitrary factor to target desired curve
+const secondsPerYear = 31556952 // 365.2425 * 86400
 
 // calculate node account bond units
 func calculateNodeAccountBondUints(height, activeBlock, slashPts int64) sdk.Uint {
@@ -42,7 +41,7 @@ func calcNodeRewards(naBlocks, totalUnits, totalRuneReward sdk.Uint) sdk.Uint {
 	return reward
 }
 
-// calculate pool rewards
+// Calculate pool rewards
 func calcPoolRewards(totalPoolRewards, totalStakedRune sdk.Uint, pools []Pool) []sdk.Uint {
 	var amts []sdk.Uint
 	for _, pool := range pools {
@@ -51,18 +50,40 @@ func calcPoolRewards(totalPoolRewards, totalStakedRune sdk.Uint, pools []Pool) [
 		)))
 		amts = append(amts, amt)
 	}
-
 	return amts
 }
 
-// calculate the block rewards that bonders and stakers should receive
-func calcBlockRewards(totalReserve sdk.Uint) (sdk.Uint, sdk.Uint) {
-	blocksPerYear := 31536000 / cmd.SecondsPerBlock
+// Calculate pool deficit based on the pool's accrued fees compared with total fees.
+func calcPoolDeficit(stakerDeficit, totalFees sdk.Uint, poolFees sdk.Uint) sdk.Uint {
+	var amt sdk.Uint
+	amt := sdk.NewUint(uint64(math.Round(
+		float64(stakerDeficit.Uint64()) / (float64(totalFees.Uint64()) / float64(poolFees.Uint64())),
+	)))
+	return amt
+}
+
+// Calculate the block rewards that bonders and stakers should receive
+func calcBlockRewards(totalReserve sdk.Uint, totalLiquidityFees sdk.Uint) (sdk.Uint, sdk.Uint, sdk.Uint) {
+	blocksPerYear := secondsPerYear / cmd.SecondsPerBlock
 	blockRewards := sdk.NewUint(uint64(
 		(float64(totalReserve.Uint64()) / float64(emissionCurve)) / float64(blocksPerYear),
 	))
-	poolReward := blockRewards.QuoUint64(3)
-	bondReward := blockRewards.Sub(poolReward)
 
-	return bondReward, poolReward
+	systemIncome = blockRewards.Add(totalLiquidityFees) // Get total system income for block
+	stakerSplit := systemIncome.QuoUint64(3)            // 1/3rd to Stakers
+	bonderSplit := systemIncome.Sub(stakerSplit)        // 2/3rd to Bonders
+
+	stakerDeficit := sdk.ZeroUint()
+	if stakerSplit >= totalLiquidityFees {
+		// Stakers have not been paid enough already, pay more
+		poolReward := stakerSplit.Sub(totalLiquidityFees) // Get how much to divert to add to staker split
+	} else {
+		// Stakers have been paid too much, calculate deficit
+		stakerDeficit := totalLiquidityFees.Sub(stakerSplit) // Deduct existing income from split
+		poolReward := sdk.ZeroUint()                         // Nothing to pay stakers now
+	}
+
+	bondReward := bonderSplit // Give bonders their cut
+
+	return bondReward, poolReward, stakerDeficit
 }
