3cd89b8e19892ad866a7e0be0c0501fff1ee9928
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-11-15 11:34:43 +1100

    refactor gas to adjust at end of the block using block rewards to refund the pools where gas is used

--- common/gas_test.go
+++ common/gas_test.go
@@ -1,18 +1,15 @@
 package common
 
 import (
-	"fmt"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	. "gopkg.in/check.v1"
 )
 
-type GSuite struct{}
+type GasSuite struct{}
 
-var _ = Suite(&GSuite{})
+var _ = Suite(&GasSuite{})
 
-func (s *GSuite) TestMultiGasCalc(c *C) {
-	fmt.Println("FOO1")
+func (s *GasSuite) TestMultiGasCalc(c *C) {
 	gas := GetBNBGasFeeMulti(1)
 	amt := gas[0].Amount
 	c.Check(
@@ -22,7 +19,6 @@ func (s *GSuite) TestMultiGasCalc(c *C) {
 		Commentf("%d", amt.Uint64()),
 	)
 
-	fmt.Println("BAR1")
 	gas = GetBNBGasFeeMulti(3)
 	amt = gas[0].Amount
 	c.Check(
@@ -31,4 +27,21 @@ func (s *GSuite) TestMultiGasCalc(c *C) {
 		true,
 		Commentf("%d", amt.Uint64()),
 	)
+}
+
+func (s *GasSuite) TestCombineGas(c *C) {
+	gas1 := Gas{
+		{Asset: BNBAsset, Amount: sdk.NewUint(11 * One)},
+	}
+	gas2 := Gas{
+		{Asset: BNBAsset, Amount: sdk.NewUint(14 * One)},
+		{Asset: BTCAsset, Amount: sdk.NewUint(20 * One)},
+	}
+
+	gas := gas1.Add(gas2)
+	c.Assert(gas, HasLen, 2)
+	c.Check(gas[0].Asset.Equals(BNBAsset), Equals, true)
+	c.Check(gas[0].Amount.Equal(sdk.NewUint(25*One)), Equals, true, Commentf("%d", gas[0].Amount.Uint64()))
+	c.Check(gas[1].Asset.Equals(BTCAsset), Equals, true)
+	c.Check(gas[1].Amount.Equal(sdk.NewUint(20*One)), Equals, true)
 }
