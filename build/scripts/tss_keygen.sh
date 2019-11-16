05210c25d84c7d393b0c551612f96c44882699c9
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-11-16 16:55:31 +1100

    tweaks

--- build/scripts/tss_keygen.sh
+++ build/scripts/tss_keygen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-make -p ~/.tss/config
+set -ex
+
+mkdir -p $(dirname $CONFIGFILE)
 
 echo "{\"parties\":\"$PARTIES\",\"threshold\":\"$THRESHOLD\",\"bootstrapnode\":\"$BOOTSTRAPNODE\",\"signerserver\":\"$SIGNERSERVER\",\"keygenserver\":\"$KEYGENSERVER\",\"partynum\":\"$PARTYNUM\",\"nodeid\":\"$TSS_NODE_ID\"}" > $CONFIGFILE
 
