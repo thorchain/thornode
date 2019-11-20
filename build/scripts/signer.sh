5094a26371bf37cdb51de489b597097ce2cca59b
Chad Barraford <cbarraford@gmail.com>
2019-11-20 15:45:04 +1100

    observer/signer tweaks

--- build/scripts/signer.sh
+++ build/scripts/signer.sh
@@ -4,10 +4,9 @@ set -ex
 . $(dirname "$0")/core.sh
 
 CHAIN_ID="${CHAIN_ID:=thorchain}"
-DEX_HOST="${DEX_HOST:=testnet-dex.binance.org}"
+BINANCE_HOST="${BINANCE_HOST:=https://data-seed-pre-0-s3.binance.org}"
 DB_PATH="${DB_PATH:=/var/data}"
 CHAIN_HOST="${CHAIN_HOST:=127.0.0.1:1317}"
-RPC_HOST="${RPC_HOST:=127.0.0.1:26657}"
 SIGNER_NAME="${SIGNER_NAME:=statechain}"
 SIGNER_PASSWD="${SIGNER_PASSWD:=password}"
 START_BLOCK_HEIGHT="${START_BLOCK_HEIGHT:=1}"
@@ -15,7 +14,7 @@ USE_TSS="${USE_TSS:=false}"
 TSS_SCHEME="${TSS_SCHEME:=http}"
 TSS_HOST="${TSS_HOST:=127.0.0.1}"
 TSS_PORT="${TSS_PORT:=8321}"
-NODE_ID="${NODE_ID:=none}"
+NODE_ID="${NODE_ID:=null}"
 
 $(dirname "$0")/wait-for-statechain-api.sh $CHAIN_HOST
 
@@ -29,10 +28,10 @@ mkdir -p /etc/observe/signd
 # Generate Signer config file
 echo "{
   \"chain_id\": \"$CHAIN_ID\",
-  \"dex_host\": \"$DEX_HOST\",
+  \"binance_host\": \"$BINANCE_HOST\",
   \"observer_db_path\": \"$OBSERVER_PATH\",
   \"block_scanner\": {
-    \"rpc_host\": \"$RPC_HOST\",
+    \"rpc_host\": \"$BINANCE_HOST\",
     \"start_block_height\": $START_BLOCK_HEIGHT,
     \"enforce_block_height\": false,
     \"block_scan_processors\": 1,
@@ -52,20 +51,20 @@ echo "{
   \"signer_db_path\": \"$SIGNER_PATH\",
   \"binance\": {
     \"private_key\": \"$BINANCE_PRIVATE_KEY\",
-    \"dex_host\": \"$DEX_HOST\"
+    \"rpc_host\": \"$BINANCE_HOST\"
   },
   \"use_tss\": $USE_TSS,
   \"key_gen\": {
     \"scheme\": \"$TSS_SCHEME\",
     \"host\": \"$TSS_HOST\",
     \"port\": $TSS_PORT,
-    \"node_id\": $NODE_ID,
+    \"node_id\": $NODE_ID
   },
   \"key_sign\": {
     \"scheme\": \"$TSS_SCHEME\",
     \"host\": \"$TSS_HOST\",
     \"port\": $TSS_PORT,
-    \"node_id\": $NODE_ID,
+    \"node_id\": $NODE_ID
   }
 }" > /etc/observe/signd/config.json
 
