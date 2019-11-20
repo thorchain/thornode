6029c8d0cea1597ba7e7b198c0edddcb773a1510
Johnny <johnnyluo1980@gmail.com>
2019-11-20 09:00:51 +0000

    202-issue Don't fund yggdrasil pool if we have less than MinimumNodesForYggdrasil

--- constants/constants.go
+++ constants/constants.go
@@ -7,4 +7,5 @@ const EmissionCurve = 6       // An arbitrary factor to target desired curve
 const BlocksPerYear = 6311390 // (365.2425 * 86400) / (Seconds per THORChain block) -> 31556952 / 5 -> 6311390
 
 // A new pool is enabled on a cycle
-const NewPoolCycle = 50000 // Enable a pool every 50,000 blocks (~3 days)
+const NewPoolCycle = 50000        // Enable a pool every 50,000 blocks (~3 days)
+const MinmumNodesForYggdrasil = 6 // No yggdrasil pools if we have less than 6 active nodes
