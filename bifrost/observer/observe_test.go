9a02d2574722cddf2c54fb8825bfb968be5653c3
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-11-21 23:43:15 +1100

    fix tests

--- bifrost/observer/observe_test.go
+++ bifrost/observer/observe_test.go
@@ -2,37 +2,39 @@ package observer
 
 import (
 	"crypto/tls"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"testing"
+
+	. "gopkg.in/check.v1"
 )
 
 const (
 	expectedHeight = int64(123456789)
 )
 
+type ObserverSuite struct{}
+
+var _ = Suite(&ObserverSuite{})
+
 // TestBinanceHeight : Test to ensure we get back the height we expect.
-func TestBinanceHeight(t *testing.T) {
+func (s *ObserverSuite) TestBinanceHeight(c *C) {
 	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch r.RequestURI {
 		case "/abci_info":
-			if _, err := w.Write([]byte(`{ "jsonrpc": "2.0", "id": "", "result": { "response": { "data": "BNBChain", "last_block_height": "123456789", "last_block_app_hash": "pwx4TJjXu3yaF6dNfLQ9F4nwAhjIqmzE8fNa+RXwAzQ=" } } }`)); nil != err {
-				t.Error(err)
-			}
+			_, err := w.Write([]byte(`{ "jsonrpc": "2.0", "id": "", "result": { "response": { "data": "BNBChain", "last_block_height": "123456789", "last_block_app_hash": "pwx4TJjXu3yaF6dNfLQ9F4nwAhjIqmzE8fNa+RXwAzQ=" } } }`))
+			c.Assert(err, IsNil)
 		}
 	})
 
-	s := httptest.NewTLSServer(h)
-	defer s.Close()
+	ser := httptest.NewServer(h)
+	defer ser.Close()
 
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 
 	client := &http.Client{Transport: tr}
-	height := binanceHeight(fmt.Sprintf("https://%s", s.Listener.Addr().String()), *client)
-	if height != expectedHeight {
-		t.Errorf("Got a height of %v but expected %v!", height, expectedHeight)
-	}
+	height, err := binanceHeight(ser.URL, *client)
+	c.Assert(err, IsNil)
+	c.Check(height, Equals, expectedHeight)
 }
