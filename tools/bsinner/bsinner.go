214c73b4cd9faaf539fc97cdb6c5ad68429c9019
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-11-24 18:41:49 +1100

    tweaks

--- tools/bsinner/bsinner.go
+++ tools/bsinner/bsinner.go
@@ -8,15 +8,13 @@ import (
 	"gitlab.com/thorchain/bepswap/thornode/test/smoke"
 )
 
-// smoke test run a json config file that is a series of transaction and expected results.
 func main() {
-	apiAddr := flag.String("a", "http://data-seed-pre-0-s3.binance.org/", "Binance RPC Address.")
+	apiAddr := flag.String("a", "https://data-seed-pre-0-s3.binance.org/", "Binance RPC address.")
 	faucetKey := flag.String("f", "", "The faucet private key.")
 	poolKey := flag.String("k", "", "The pool key.")
-	environment := flag.String("e", "stage", "The environment to use [local|staging|develop|production].")
+	environment := flag.String("e", "local", "The environment to use [local|staging|develop|production]. Defaults to local")
 	bal := flag.String("b", "", "Balances json file")
 	txns := flag.String("t", "", "Transactions json file")
-	sweep := flag.Bool("s", false, "Sweep funds back on exit [Default: false]")
 	fastFail := flag.Bool("x", false, "Enable fast fail")
 	debug := flag.Bool("d", false, "Enable debugging of the Binance transactions.")
 	flag.Parse()
@@ -29,11 +27,7 @@ func main() {
 		log.Fatal("No balances json file")
 	}
 
-	if *faucetKey == "" {
-		log.Fatal("No faucet key set!")
-	}
-
-	s := smoke.NewSmoke(*apiAddr, *faucetKey, *poolKey, *environment, *bal, *txns, *sweep, *fastFail, *debug)
+	s := smoke.NewSmoke(*apiAddr, *faucetKey, *poolKey, *environment, *bal, *txns, *fastFail, *debug)
 	successful := s.Run()
 	if successful {
 		os.Exit(0)
