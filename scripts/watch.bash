1d04cc1442371d5cd172fdc12e80010beaa0c9ee
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-11-26 15:41:21 +1100

    add tests

--- scripts/watch.bash
+++ scripts/watch.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-make test ; fswatch . -e ".*" -i "\\.go$" --event PlatformSpecific | xargs -n1 -I{}  make clear test
+make test ; fswatch . -e ".*" -i "\\.go$" | xargs -n1 -I{}  make clear test
