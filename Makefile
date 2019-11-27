4f19b373be67896392177e0db28c1cc65da60719
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-11-27 12:08:41 +1100

    switch to genesis with TSS for smoke tests

--- Makefile
+++ Makefile
@@ -83,10 +83,8 @@ smoke-test: tools install
 	./build/scripts/smoke.sh
 
 smoke-local:
+	make -C build/docker run-genesisWithMockBinance
 	bsinner -a localhost:26660 -b ./test/smoke/scenarios/balances.json -t ./test/smoke/scenarios/transactions.json -e local -x
-
-smoke-ci:
-	STOP_ID=6 bsinner -a docker:26660 -b ./test/smoke/scenarios/balances.json -t ./test/smoke/scenarios/transactions.json -e ci -x
 
 export:
 	thord export
--- build/scripts/observer.sh
+++ build/scripts/observer.sh
@@ -3,13 +3,13 @@ set -ex
 
 CHAIN_ID="${CHAIN_ID:=thorchain}"
 DB_PATH="${DB_PATH:=/var/data}"
-CHAIN_HOST="${CHAIN_HOST:=127.0.0.1:1317}"
+CHAIN_API="${CHAIN_API:=127.0.0.1:1317}"
 RPC_SCHEME="${RPC_RPC_SCHEME:=https}"
 BINANCE_HOST="${BINANCE_HOST:=https://data-seed-pre-0-s3.binance.org}"
 SIGNER_NAME="${SIGNER_NAME:=statechain}"
 SIGNER_PASSWD="${SIGNER_PASSWD:=password}"
 
-$(dirname "$0")/wait-for-statechain-api.sh $CHAIN_HOST
+$(dirname "$0")/wait-for-statechain-api.sh $CHAIN_API
 
 OBSERVER_PATH=$DB_PATH/observer/
 mkdir -p $OBSERVER_PATH
@@ -29,7 +29,7 @@ echo "{
   },
   \"state_chain\": {
     \"chain_id\": \"$CHAIN_ID\",
-    \"chain_host\": \"$CHAIN_HOST\",
+    \"chain_host\": \"$CHAIN_API\",
     \"signer_name\": \"$SIGNER_NAME\",
     \"signer_passwd\": \"$SIGNER_PASSWD\"
   },
