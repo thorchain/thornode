e9585a53dc508a1c630f95891c18aecde0480343
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-11-29 19:30:56 +1100

    remove -alpha -beta

--- x/thorchain/types/msg_version_test.go
+++ x/thorchain/types/msg_version_test.go
@@ -12,7 +12,7 @@ var _ = Suite(&MsgSetVersionSuite{})
 func (MsgSetVersionSuite) TestMsgSetVersionSuite(c *C) {
 	acc1 := GetRandomBech32Addr()
 	c.Assert(acc1.Empty(), Equals, false)
-	msg := NewMsgSetVersion(semver.MustParse("2.0.0-beta"), acc1)
+	msg := NewMsgSetVersion(semver.MustParse("2.0.0"), acc1)
 	c.Assert(msg.Route(), Equals, RouterKey)
 	c.Assert(msg.Type(), Equals, "set_version")
 	c.Assert(msg.ValidateBasic(), IsNil)
