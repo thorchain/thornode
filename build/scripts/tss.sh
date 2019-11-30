c3af23acead1999237895c9fed8c5dbe86d7d2e2
Chad Barraford <cbarraford@gmail.com>
2019-11-30 14:32:29 +1100

    new tss script

---
+++ build/scripts/tss.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# wait for our private key
+while [ ! -f $PRIVKEY ]; do
+    sleep 3
+done
+
+if [ -z "$SEED" ]; then
+    while ! nc -z $SEED 4040; do
+        sleep 1
+    done
+
+    echo $PRIVKEY | /go/bin/tss -http 4040 -peer /ip4/$SEED/tcp/5040/ipfs/$(curl http://$SEED:4040/p2pid) -port 5040
+
+else
+    echo $PRIVKEY | /go/bin/tss -http 4040 -port 5040
+fi
--- build/scripts/tss_keygen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-mkdir -p $(dirname $CONFIGFILE)
-
-echo "{\"parties\":\"$PARTIES\",\"threshold\":\"$THRESHOLD\",\"bootstrapnode\":\"$BOOTSTRAPNODE\",\"signerserver\":\"$SIGNERSERVER\",\"keygenserver\":\"$KEYGENSERVER\",\"partynum\":\"$PARTYNUM\",\"nodeid\":\"$TSS_NODE_ID\"}" > $CONFIGFILE
-
-exec "$@"
