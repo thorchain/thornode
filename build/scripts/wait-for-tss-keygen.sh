81526b5dd7c6a2de568673b060c7525e47cfbe0d
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-11-30 21:06:14 +1100

    tweaks

--- build/scripts/wait-for-tss-keygen.sh
+++ build/scripts/wait-for-tss-keygen.sh
@@ -6,8 +6,8 @@ set -xe
 echo "Waiting for TSS Keygen..."
 
 until curl -s "$1:4040"; do
-  echo "TSS Keysign is unavailable - sleeping ($1:4040)"
+  echo "TSS Keygen is unavailable - sleeping ($1:4040)"
   sleep 3
 done
 
-echo "TSS Keysign is up!"
+echo "TSS Keygen is available!"
