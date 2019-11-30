9e00f12c4c3856840c18ec24ef868cc5249da36f
Chad Barraford <cbarraford@gmail.com>
2019-11-30 21:06:14 +1100

    tweaks

--- build/scripts/tss.sh
+++ build/scripts/tss.sh
@@ -8,8 +8,6 @@ while [ ! -f $PRIVKEY ]; do
 done
 
 if [ ! -z ${SEED+x} ]; then
-    echo $PRIVKEY
-    cat $PRIVKEY
     while ! nc -z $SEED 4040; do
         sleep 1
     done
@@ -17,7 +15,5 @@ if [ ! -z ${SEED+x} ]; then
     cat $PRIVKEY | /go/bin/tss -http 4040 -peer /ip4/$SEED/tcp/5040/ipfs/$(curl http://$SEED:4040/p2pid) -port 5040
 
 else
-    echo $PRIVKEY
-    cat $PRIVKEY
     cat $PRIVKEY | /go/bin/tss -http 4040 -port 5040
 fi
