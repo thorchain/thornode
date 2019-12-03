f5e78257cdf907e5eabb8917f3786a19beb16696
Heimdall <heimdallthor02@gmail.com>
2019-12-04 07:51:40 +1100

    223-issue refactor keeper_txout

--- x/thorchain/keeper_txout.go
+++ x/thorchain/keeper_txout.go
@@ -4,20 +4,24 @@ import (
 	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/pkg/errors"
 )
 
 type KeeperTxOut interface {
-	SetTxOut(ctx sdk.Context, blockOut *TxOut)
+	SetTxOut(ctx sdk.Context, blockOut *TxOut) error
 	GetTxOutIterator(ctx sdk.Context) sdk.Iterator
 	GetTxOut(ctx sdk.Context, height uint64) (*TxOut, error)
 }
 
 // SetTxOut - write the given txout information to key values tore
-func (k KVStore) SetTxOut(ctx sdk.Context, blockOut *TxOut) {
+func (k KVStore) SetTxOut(ctx sdk.Context, blockOut *TxOut) error {
 	store := ctx.KVStore(k.storeKey)
 	key := k.GetKey(ctx, prefixTxOut, strconv.FormatUint(blockOut.Height, 10))
-	store.Set([]byte(key), k.cdc.MustMarshalBinaryBare(blockOut))
+	buf, err := k.cdc.MarshalBinaryBare(blockOut)
+	if nil != err {
+		return dbError(ctx, "fail to marshal tx out to binary", err)
+	}
+	store.Set([]byte(key), buf)
+	return nil
 }
 
 // GetTxOutIterator iterate tx out
@@ -36,7 +40,7 @@ func (k KVStore) GetTxOut(ctx sdk.Context, height uint64) (*TxOut, error) {
 	buf := store.Get([]byte(key))
 	var txOut TxOut
 	if err := k.cdc.UnmarshalBinaryBare(buf, &txOut); nil != err {
-		return nil, errors.Wrap(err, "fail to unmarshal tx out")
+		return nil, dbError(ctx, "fail to unmarshal tx out", err)
 	}
 	return &txOut, nil
 }
