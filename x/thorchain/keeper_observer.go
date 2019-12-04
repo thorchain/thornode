72cf70cfdc00457e5b91e98600c537ff52381334
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-12-04 13:59:15 +1100

    refactor observer keeper

--- x/thorchain/keeper_observer.go
+++ x/thorchain/keeper_observer.go
@@ -6,8 +6,8 @@ type KeeperObserver interface {
 	SetActiveObserver(ctx sdk.Context, addr sdk.AccAddress)
 	RemoveActiveObserver(ctx sdk.Context, addr sdk.AccAddress)
 	IsActiveObserver(ctx sdk.Context, addr sdk.AccAddress) bool
-	GetObservingAddresses(ctx sdk.Context) []sdk.AccAddress
-	AddObservingAddresses(ctx sdk.Context, inAddresses []sdk.AccAddress)
+	GetObservingAddresses(ctx sdk.Context) ([]sdk.AccAddress, error)
+	AddObservingAddresses(ctx sdk.Context, inAddresses []sdk.AccAddress) error
 	ClearObservingAddresses(ctx sdk.Context)
 }
 
@@ -37,25 +37,31 @@ func (k KVStore) IsActiveObserver(ctx sdk.Context, addr sdk.AccAddress) bool {
 // GetObservingAddresses - get list of observed addresses. This is a list of
 // addresses that have recently contributed via observing a tx that got 2/3rds
 // majority
-func (k KVStore) GetObservingAddresses(ctx sdk.Context) []sdk.AccAddress {
+func (k KVStore) GetObservingAddresses(ctx sdk.Context) ([]sdk.AccAddress, error) {
 	key := k.GetKey(ctx, prefixObservingAddresses, "")
 
 	store := ctx.KVStore(k.storeKey)
 	if !store.Has([]byte(key)) {
-		return nil
+		return make([]sdk.AccAddress, 0), nil
 	}
 
 	bz := store.Get([]byte(key))
 	var addresses []sdk.AccAddress
-	k.cdc.MustUnmarshalBinaryBare(bz, &addresses)
-	return addresses
+	if err := k.cdc.UnmarshalBinaryBare(bz, &addresses); err != nil {
+		return nil, dbError(ctx, "Unmarshal: observer", err)
+	}
+	return addresses, nil
 }
 
 // AddObservingAddresses - add a list of addresses that have been helpful in
 // getting enough observations to process an inbound tx.
-func (k KVStore) AddObservingAddresses(ctx sdk.Context, inAddresses []sdk.AccAddress) {
+func (k KVStore) AddObservingAddresses(ctx sdk.Context, inAddresses []sdk.AccAddress) error {
 	// combine addresses
-	all := append(k.GetObservingAddresses(ctx), inAddresses...)
+	curr, err := k.GetObservingAddresses(ctx)
+	if err != nil {
+		return err
+	}
+	all := append(curr, inAddresses...)
 
 	// ensure uniqueness
 	uniq := make([]sdk.AccAddress, 0, len(all))
@@ -70,6 +76,7 @@ func (k KVStore) AddObservingAddresses(ctx sdk.Context, inAddresses []sdk.AccAdd
 	store := ctx.KVStore(k.storeKey)
 	key := k.GetKey(ctx, prefixObservingAddresses, "")
 	store.Set([]byte(key), k.cdc.MustMarshalBinaryBare(uniq))
+	return nil
 }
 
 // ClearObservingAddresses - clear all observing addresses
