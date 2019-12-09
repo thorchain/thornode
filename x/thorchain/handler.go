54978ecd54de9bac86729b5d163312a9a1df5d58
Adam Guldemann <adam.guldemann@gmail.com>
2019-12-09 18:05:01 +0100

    [Refactor] Add yggdrasil handler

--- x/thorchain/handler.go
+++ x/thorchain/handler.go
@@ -2,7 +2,6 @@ package thorchain
 
 import (
 	"encoding/json"
-	stdErrors "errors"
 	"fmt"
 
 	"github.com/blang/semver"
@@ -47,6 +46,7 @@ func getHandlerMapping(keeper Keeper, poolAddrMgr PoolAddressManager, txOutStore
 	// New arch handlers
 	m := make(map[string]MsgHandler)
 	m[MsgNoOp{}.Type()] = NewNoOpHandler(keeper)
+	m[MsgYggdrasil{}.Type()] = NewYggdrasilHandler(keeper, txOutStore, poolAddrMgr, validatorMgr)
 	m[MsgReserveContributor{}.Type()] = NewReserveContributorHandler(keeper)
 	m[MsgSetPoolData{}.Type()] = NewPoolDataHandler(keeper)
 	m[MsgSetVersion{}.Type()] = NewVersionHandler(keeper)
@@ -76,8 +76,6 @@ func NewClassicHandler(keeper Keeper, poolAddressMgr PoolAddressManager, txOutSt
 			return handleOperatorMsgEndPool(ctx, keeper, txOutStore, poolAddressMgr, m)
 		case MsgSetTrustAccount:
 			return handleMsgSetTrustAccount(ctx, keeper, m)
-		case MsgYggdrasil:
-			return handleMsgYggdrasil(ctx, keeper, txOutStore, poolAddressMgr, validatorManager, m)
 		default:
 			errMsg := fmt.Sprintf("Unrecognized thorchain Msg type: %v", m)
 			return sdk.ErrUnknownRequest(errMsg).Result()
@@ -610,134 +608,4 @@ func handleMsgSetTrustAccount(ctx sdk.Context, keeper Keeper, msg MsgSetTrustAcc
 		Code:      sdk.CodeOK,
 		Codespace: DefaultCodespace,
 	}
-}
-
-// handleMsgYggdrasil
-func handleMsgYggdrasil(ctx sdk.Context, keeper Keeper, txOut TxOutStore, poolAddrMgr PoolAddressManager, validatorMgr ValidatorManager, msg MsgYggdrasil) sdk.Result {
-	ctx.Logger().Info("receive MsgYggdrasil", "pubkey", msg.PubKey.String(), "add_funds", msg.AddFunds, "coins", msg.Coins)
-
-	if !isSignedByActiveObserver(ctx, keeper, msg.GetSigners()) {
-		ctx.Logger().Error("message signed by unauthorized account", "signer", msg.GetSigners())
-		return sdk.ErrUnauthorized("Not authorized").Result()
-	}
-	if err := msg.ValidateBasic(); nil != err {
-		ctx.Logger().Error("invalid MsgYggdrasil", "error", err)
-		return sdk.ErrUnknownRequest(err.Error()).Result()
-	}
-
-	ygg, err := keeper.GetYggdrasil(ctx, msg.PubKey)
-	if nil != err && !stdErrors.Is(err, ErrYggdrasilNotFound) {
-		ctx.Logger().Error("fail to get yggdrasil", err)
-		return sdk.ErrInternal("fail to get yggdrasil").Result()
-	}
-	if msg.AddFunds {
-		ygg.AddFunds(msg.Coins)
-	} else {
-		ygg.SubFunds(msg.Coins)
-		ctx.EventManager().EmitEvent(
-			sdk.NewEvent("yggdrasil_return",
-				sdk.NewAttribute("pubkey", ygg.PubKey.String()),
-				sdk.NewAttribute("coins", msg.Coins.String()),
-				sdk.NewAttribute("tx", msg.RequestTxHash.String())))
-
-		na, err := keeper.GetNodeAccountByPubKey(ctx, msg.PubKey)
-		if err != nil {
-			ctx.Logger().Error("unable to get node account", "error", err)
-			return sdk.ErrUnknownRequest(err.Error()).Result()
-		}
-		// if the node account requested to leave already ,then let's refund them
-		if na.RequestedToLeave {
-			if err := refundBond(ctx, msg.RequestTxHash, na, keeper, txOut); err != nil {
-				ctx.Logger().Error("fail to refund bond", err)
-				return sdk.ErrInternal("fail to refund bond").Result()
-			}
-		}
-	}
-	if err := keeper.SetYggdrasil(ctx, ygg); nil != err {
-		ctx.Logger().Error("fail to save yggdrasil", err)
-		return sdk.ErrInternal("fail to save yggdrasil").Result()
-	}
-
-	// Ragnarok protocol get triggered, if all the Yggdrasil pool returned funds already, THORNode will continue Ragnarok
-	if validatorMgr.Meta().Ragnarok {
-		hasYggdrasilPool, err := keeper.HasValidYggdrasilPools(ctx)
-		if nil != err {
-			return sdk.ErrInternal(fmt.Errorf("fail to check yggdrasil pools: %w", err).Error()).Result()
-		}
-		if !hasYggdrasilPool {
-			return handleRagnarokProtocolStep2(ctx, keeper, txOut, poolAddrMgr, validatorMgr)
-		}
-	}
-	return sdk.Result{
-		Code:      sdk.CodeOK,
-		Codespace: DefaultCodespace,
-	}
-}
-
-func handleRagnarokProtocolStep2(ctx sdk.Context, keeper Keeper, txOut TxOutStore, poolAddrMgr PoolAddressManager, validatorManager ValidatorManager) sdk.Result {
-	// Ragnarok Protocol
-	// If THORNode can no longer be BFT, do a graceful shutdown of the entire network.
-	// 1) THORNode will request all yggdrasil pool to return fund , if THORNode don't have yggdrasil pool THORNode will go to step 3 directly
-	// 2) upon receiving the yggdrasil fund,  THORNode will refund the validator's bond
-	// 3) once all yggdrasil fund get returned, return all fund to stakes
-	if !validatorManager.Meta().Ragnarok {
-		// Ragnarok protocol didn't triggered , don't call this one
-		return sdk.Result{
-			Code:      sdk.CodeOK,
-			Codespace: DefaultCodespace,
-		}
-	}
-	// get the first observer
-	nas, err := keeper.ListActiveNodeAccounts(ctx)
-	if nil != err {
-		ctx.Logger().Error("can't get active nodes", err)
-		return sdk.ErrInternal("can't get active nodes").Result()
-	}
-	if len(nas) == 0 {
-		return sdk.ErrInternal("can't find any active nodes").Result()
-	}
-
-	pools, err := keeper.GetPools(ctx)
-	if err != nil {
-		return sdk.ErrInternal(err.Error()).Result()
-	}
-
-	// go through all the pooles
-	for _, pool := range pools {
-		poolStaker, err := keeper.GetPoolStaker(ctx, pool.Asset)
-		if nil != err {
-			ctx.Logger().Error("fail to get pool staker", err)
-			return sdk.ErrInternal(err.Error()).Result()
-		}
-
-		// everyone withdraw
-		for _, item := range poolStaker.Stakers {
-			unstakeMsg := NewMsgSetUnStake(
-				common.GetRagnarokTx(pool.Asset.Chain),
-				item.RuneAddress,
-				sdk.NewUint(10000),
-				pool.Asset,
-				nas[0].NodeAddress,
-			)
-			unstakeHandler := NewUnstakeHandler(keeper, txOut, poolAddrMgr)
-			ver := semver.MustParse("0.1.0")
-			result := unstakeHandler.Run(ctx, unstakeMsg, ver)
-			if !result.IsOK() {
-				ctx.Logger().Error("fail to unstake", "staker", item.RuneAddress)
-				return result
-			}
-		}
-		pool.Status = PoolSuspended
-		if err := keeper.SetPool(ctx, pool); err != nil {
-			err = errors.Wrap(err, "fail to set pool")
-			ctx.Logger().Error(err.Error())
-			return sdk.ErrInternal(err.Error()).Result()
-		}
-	}
-
-	return sdk.Result{
-		Code:      sdk.CodeOK,
-		Codespace: DefaultCodespace,
-	}
-
 }
