887660c5336df8ec2052580fc8234428f26a346c
Son of Odin <dev@thorchain.org>
2019-12-11 00:02:35 +1100

    set node status to ready

--- x/thorchain/handler.go
+++ x/thorchain/handler.go
@@ -586,10 +586,9 @@ func handleMsgSetTrustAccount(ctx sdk.Context, keeper Keeper, msg MsgSetTrustAcc
 	}
 	// Here make sure THORNode don't change the node account's bond
 
-	nodeAccount.Status = NodeReady // TODO: should check version is set, and observer is ready monitoring chains
 	nodeAccount.ValidatorConsPubKey = msg.ValidatorConsPubKey
 	nodeAccount.NodePubKey = msg.NodePubKeys
-	nodeAccount.UpdateStatus(NodeStandby, ctx.BlockHeight())
+	nodeAccount.UpdateStatus(NodeReady, ctx.BlockHeight())
 	if err := keeper.SetNodeAccount(ctx, nodeAccount); nil != err {
 		ctx.Logger().Error(fmt.Sprintf("fail to save node account: %s", nodeAccount), err)
 		return sdk.ErrInternal("fail to save node account").Result()
