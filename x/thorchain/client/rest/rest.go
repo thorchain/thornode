9e78d0eddf94356b39ebaee9b4dc3ae12c4a9ce0
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-12-15 16:26:36 +1100

    add newTss handler to be routed

--- x/thorchain/client/rest/rest.go
+++ x/thorchain/client/rest/rest.go
@@ -50,6 +50,10 @@ func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, storeName string) 
 		fmt.Sprintf("/%s/txs", storeName),
 		postTxsHandler(cliCtx),
 	).Methods(http.MethodPost)
+	r.HandleFunc(
+		fmt.Sprintf("/%s/tss", storeName),
+		newTssPoolHandler(cliCtx),
+	).Methods(http.MethodPost)
 	r.Use(mux.CORSMethodMiddleware(r))
 	r.Use(customCORSHeader())
 }
