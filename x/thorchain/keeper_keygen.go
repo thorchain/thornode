ff076dbe06193febdc5505285a0da838e0243140
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-12-17 16:10:48 +1100

    tweaks

--- x/thorchain/keeper_keygen.go
+++ x/thorchain/keeper_keygen.go
@@ -12,25 +12,22 @@ type KeeperKeygen interface {
 	GetKeygens(ctx sdk.Context, height uint64) (Keygens, error)
 }
 
-// SetKeygen - write the given txout information to key values tore
 func (k KVStore) SetKeygens(ctx sdk.Context, keygens Keygens) error {
 	store := ctx.KVStore(k.storeKey)
 	key := k.GetKey(ctx, prefixKeygen, strconv.FormatUint(keygens.Height, 10))
 	buf, err := k.cdc.MarshalBinaryBare(keygens)
 	if nil != err {
-		return dbError(ctx, "fail to marshal tx out to binary", err)
+		return dbError(ctx, "fail to marshal keygens", err)
 	}
 	store.Set([]byte(key), buf)
 	return nil
 }
 
-// GetKeygenIterator iterate tx out
 func (k KVStore) GetKeygensIterator(ctx sdk.Context) sdk.Iterator {
 	store := ctx.KVStore(k.storeKey)
 	return sdk.KVStorePrefixIterator(store, []byte(prefixKeygen))
 }
 
-// GetKeygen - write the given txout information to key values tore
 func (k KVStore) GetKeygens(ctx sdk.Context, height uint64) (Keygens, error) {
 	store := ctx.KVStore(k.storeKey)
 	key := k.GetKey(ctx, prefixKeygen, strconv.FormatUint(height, 10))
@@ -40,7 +37,7 @@ func (k KVStore) GetKeygens(ctx sdk.Context, height uint64) (Keygens, error) {
 	buf := store.Get([]byte(key))
 	var keygens Keygens
 	if err := k.cdc.UnmarshalBinaryBare(buf, &keygens); nil != err {
-		return Keygens{}, dbError(ctx, "fail to unmarshal tx out", err)
+		return Keygens{}, dbError(ctx, "fail to unmarshal keygens", err)
 	}
 	return keygens, nil
 }
