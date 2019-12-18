53a02aaf7299ae7b7cfa38c39264dce8b3d883eb
Loki Laufeyson <l031@protonmail.com>
2019-12-18 19:21:15 +1100

    * Removed code that drop tx for memo is blank. * Added tests around the processStdTx method.

--- bifrostv2/pkg/blockclients/bnb/bnb_test.go
+++ bifrostv2/pkg/blockclients/bnb/bnb_test.go
@@ -2,6 +2,8 @@ package bnb
 
 import (
 	"github.com/binance-chain/go-sdk/common/types"
+	"github.com/binance-chain/go-sdk/types/msg"
+	"github.com/binance-chain/go-sdk/types/tx"
 
 	"testing"
 
@@ -52,5 +54,61 @@ func (s *BNBSuite) TestGetBlock(c *C) {
 	ResultBlock, err := s.Client.getBlock(1)
 	c.Assert(err, IsNil)
 	c.Assert(ResultBlock, NotNil)
+}
 
+func (s *BNBSuite) TestProcessStdTx(c *C) {
+	// happy path
+	stdTx, err := getStdTx(
+		"tbnb1yycn4mh6ffwpjf584t8lpp7c27ghu03gpvqkfj",
+		"tbnb1yxfyeda8pnlxlmx0z3cwx74w9xevspwdpzdxpj",
+		types.Coins{types.Coin{Denom: "BNB", Amount: 194765912}},
+		"outbound:256",
+	)
+	c.Assert(err, IsNil)
+	c.Assert(stdTx, NotNil)
+
+	items, err := s.Client.processStdTx(stdTx)
+	c.Assert(err, IsNil)
+	c.Assert(items, NotNil)
+	c.Assert(len(items), Equals, 1)
+	c.Assert(items[0].Memo, Equals, "outbound:256")
+	c.Assert(items[0].To.String(), Equals, "tbnb1yxfyeda8pnlxlmx0z3cwx74w9xevspwdpzdxpj")
+	c.Assert(items[0].From.String(), Equals, "tbnb1yycn4mh6ffwpjf584t8lpp7c27ghu03gpvqkfj")
+
+	// no memo (will be refunded)
+	stdTx, err = getStdTx(
+		"tbnb1yycn4mh6ffwpjf584t8lpp7c27ghu03gpvqkfj",
+		"tbnb1yxfyeda8pnlxlmx0z3cwx74w9xevspwdpzdxpj",
+		types.Coins{types.Coin{Denom: "BNB", Amount: 194765912}},
+		"",
+	)
+	c.Assert(err, IsNil)
+	c.Assert(stdTx, NotNil)
+
+	items, err = s.Client.processStdTx(stdTx)
+	c.Assert(err, IsNil)
+	c.Assert(items, NotNil)
+	c.Assert(len(items), Equals, 1)
+	c.Assert(items[0].Memo, Equals, "")
+	c.Assert(items[0].To.String(), Equals, "tbnb1yxfyeda8pnlxlmx0z3cwx74w9xevspwdpzdxpj")
+	c.Assert(items[0].From.String(), Equals, "tbnb1yycn4mh6ffwpjf584t8lpp7c27ghu03gpvqkfj")
+
+	// TODO add test for other message types
+
+	// TODO add test with multiple `SendMsg` tx's.
+}
+
+func getStdTx(f, t string, coins []types.Coin, memo string) (tx.StdTx, error) {
+	types.Network = types.TestNetwork
+	from, err := types.AccAddressFromBech32(f)
+	if err != nil {
+		return tx.StdTx{}, err
+	}
+	to, err := types.AccAddressFromBech32(t)
+	if err != nil {
+		return tx.StdTx{}, err
+	}
+	transfers := []msg.Transfer{msg.Transfer{to, coins}}
+	m := msg.CreateSendMsg(from, coins, transfers)
+	return tx.NewStdTx([]msg.Msg{m}, nil, memo, 0, nil), nil
 }
