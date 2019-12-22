6c06dbd59c968ce2d3be7008811437b57c663b5e
Son of Odin <dev@thorchain.org>
2019-12-22 20:05:59 +1100

    add next pool detection

--- x/thorchain/thorchain_test.go
+++ x/thorchain/thorchain_test.go
@@ -1,6 +1,7 @@
 package thorchain
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/blang/semver"
@@ -21,9 +22,12 @@ func (s *ThorchainSuite) TestChurn(c *C) {
 	ver := semver.MustParse("0.1.0")
 	consts := constants.GetConstantValues(ver)
 
+	txOutStore := NewTxStoreDummy()
+	vaultMgr := NewVaultMgr(keeper, txOutStore)
+	validatorMgr := NewValidatorMgr(keeper, vaultMgr)
+
 	// create starting point, vault and four node active node accounts
 	vault := GetRandomVault()
-	// add funds to be migrated to the new vault
 	vault.AddFunds(common.Coins{
 		common.NewCoin(common.RuneAsset(), sdk.NewUint(100*common.One)),
 		common.NewCoin(common.BNBAsset, sdk.NewUint(79*common.One)),
@@ -33,30 +37,43 @@ func (s *ThorchainSuite) TestChurn(c *C) {
 		na := GetRandomNodeAccount(NodeActive)
 		addresses[i] = na.NodeAddress
 		na.SignerMembership = []common.PubKey{vault.PubKey}
+		if i == 0 { // give the first node account slash points
+			na.RequestedToLeave = true
+		}
 		vault.Membership = append(vault.Membership, na.NodePubKey.Secp256k1)
 		c.Assert(keeper.SetNodeAccount(ctx, na), IsNil)
 	}
 	c.Assert(keeper.SetVault(ctx, vault), IsNil)
 
-	// pretend a keygen has produced a successful new vault
-	txOutStore := NewTxStoreDummy()
-	vaultMgr := NewVaultMgr(keeper, txOutStore)
-
 	// create new node account to rotate in
 	na := GetRandomNodeAccount(NodeReady)
 	c.Assert(keeper.SetNodeAccount(ctx, na), IsNil)
 
-	// make list of pubkeys to be in the new vault. Excluding the first node
-	// account, but including the fifth node
-	pks := append(vault.Membership[1:], na.NodePubKey.Secp256k1)
+	// trigger marking bad actors as well as a keygen
+	rotateHeight := consts.GetInt64Value(constants.RotatePerBlockHeight)
+	ctx = ctx.WithBlockHeight(rotateHeight)
+	c.Assert(validatorMgr.BeginBlock(ctx, consts), IsNil)
+
+	// check we've created a keygen, with the correct members
+	keygens, err := keeper.GetKeygens(ctx, uint64(ctx.BlockHeight()))
+	c.Assert(err, IsNil)
+	c.Assert(keygens.Keygens, HasLen, 1)
+	expected := append(vault.Membership[1:], na.NodePubKey.Secp256k1)
+	// sort our slices so they are in the same order
+	sort.Slice(expected, func(i, j int) bool { return expected[i].String() < expected[j].String() })
+	sort.Slice(keygens.Keygens[0], func(i, j int) bool { return keygens.Keygens[0][i].String() < keygens.Keygens[0][j].String() })
+	c.Assert(expected, HasLen, len(keygens.Keygens[0]))
+	for i := range expected {
+		c.Assert(expected[i].Equals(keygens.Keygens[0][i]), Equals, true, Commentf("%d: %s <==> %s", i, expected[i], keygens.Keygens[0][i]))
+	}
 
 	// generate a tss keygen handler event
 	newVaultPk := GetRandomPubKey()
-	msg := NewMsgTssPool(pks, newVaultPk, addresses[0])
+	msg := NewMsgTssPool(keygens.Keygens[0], newVaultPk, addresses[0])
 	tssHandler := NewTssHandler(keeper, vaultMgr)
 
 	voter := NewTssVoter(msg.ID, msg.PubKeys, msg.PoolPubKey)
-	voter.Signers = addresses // ensure we have consensus
+	voter.Signers = addresses // ensure we have consensus, so handler is properly executed
 	keeper.SetTssVoter(ctx, voter)
 
 	result := tssHandler.Run(ctx, msg, ver, consts)
@@ -72,7 +89,6 @@ func (s *ThorchainSuite) TestChurn(c *C) {
 	c.Assert(vault2.Membership, HasLen, 4)
 
 	// check our validators get rotated appropriately
-	validatorMgr := NewValidatorMgr(keeper, vaultMgr)
 	validators := validatorMgr.EndBlock(ctx, txOutStore, consts)
 	nas, err := keeper.ListActiveNodeAccounts(ctx)
 	c.Assert(err, IsNil)
