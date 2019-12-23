7c4a14270f60e60d2a0cd3f0f3ffeef299c4de1c
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-12-23 14:43:18 +1100

    fix yggdrasil returns

--- bifrost/thorclient/types/tx_out.go
+++ bifrost/thorclient/types/tx_out.go
@@ -1,17 +1,42 @@
 package types
 
-import "gitlab.com/thorchain/thornode/common"
+import (
+	"gitlab.com/thorchain/thornode/common"
+)
 
-// TODO decomission TxArrayItem, in favor of TxOutItem in thornode
-type TxArrayItem struct {
-	Chain       common.Chain  `json:"chain"`
-	To          string        `json:"to"`
-	VaultPubKey common.PubKey `json:"vault_pubkey"`
-	Coin        common.Coin   `json:"coin"`
-	Memo        string        `json:"memo"`
-	InHash      common.TxID   `json:"in_hash"`
-	OutHash     common.TxID   `json:"out_hash"`
+type TxOutItem struct {
+	Chain       common.Chain   `json:"chain"`
+	ToAddress   common.Address `json:"to"`
+	VaultPubKey common.PubKey  `json:"vault_pubkey"`
+	SeqNo       uint64         `json:"seq_no"`
+	Coins       common.Coins   `json:"coins"`
+	Memo        string         `json:"memo"`
+	InHash      common.TxID    `json:"in_hash"`
+	OutHash     common.TxID    `json:"out_hash"`
 }
+
+type TxArrayItem struct {
+	Chain       common.Chain   `json:"chain"`
+	ToAddress   common.Address `json:"to"`
+	VaultPubKey common.PubKey  `json:"vault_pubkey"`
+	Coin        common.Coin    `json:"coin"`
+	Memo        string         `json:"memo"`
+	InHash      common.TxID    `json:"in_hash"`
+	OutHash     common.TxID    `json:"out_hash"`
+}
+
+func (tx TxArrayItem) TxOutItem() TxOutItem {
+	return TxOutItem{
+		Chain:       tx.Chain,
+		ToAddress:   tx.ToAddress,
+		VaultPubKey: tx.VaultPubKey,
+		Coins:       common.Coins{tx.Coin},
+		Memo:        tx.Memo,
+		InHash:      tx.InHash,
+		OutHash:     tx.OutHash,
+	}
+}
+
 type TxOut struct {
 	Height  string        `json:"height"`
 	Hash    string        `json:"hash"`
