e8e9a0058226f3e22119a6bb7d8b86a1ba3f7dbe
Heimdall <heimdallthor02@gmail.com>
2020-01-04 05:48:39 +0000

    Resolve "add query switch to include pending events in API"

--- x/thorchain/handler_swap.go
+++ x/thorchain/handler_swap.go
@@ -1,6 +1,8 @@
 package thorchain
 
 import (
+	"encoding/json"
+
 	"github.com/blang/semver"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -21,7 +23,7 @@ func NewSwapHandler(keeper Keeper, txOutStore TxOutStore) SwapHandler {
 	}
 }
 
-func (h SwapHandler) Run(ctx sdk.Context, m sdk.Msg, version semver.Version, _ constants.ConstantValues) sdk.Result {
+func (h SwapHandler) Run(ctx sdk.Context, m sdk.Msg, version semver.Version, constAccessor constants.ConstantValues) sdk.Result {
 	msg, ok := m.(MsgSwap)
 	if !ok {
 		return errInvalidMessage.Result()
@@ -29,7 +31,7 @@ func (h SwapHandler) Run(ctx sdk.Context, m sdk.Msg, version semver.Version, _ c
 	if err := h.validate(ctx, msg, version); err != nil {
 		return sdk.ErrInternal(err.Error()).Result()
 	}
-	return h.handle(ctx, msg, version)
+	return h.handle(ctx, msg, version, constAccessor)
 }
 
 func (h SwapHandler) validate(ctx sdk.Context, msg MsgSwap, version semver.Version) error {
@@ -54,28 +56,46 @@ func (h SwapHandler) validateV1(ctx sdk.Context, msg MsgSwap) error {
 	return nil
 }
 
-func (h SwapHandler) handle(ctx sdk.Context, msg MsgSwap, version semver.Version) sdk.Result {
+func (h SwapHandler) handle(ctx sdk.Context, msg MsgSwap, version semver.Version, constAccessor constants.ConstantValues) sdk.Result {
 	ctx.Logger().Info("receive MsgSwap", "request tx hash", msg.Tx.ID, "source asset", msg.Tx.Coins[0].Asset, "target asset", msg.TargetAsset, "signer", msg.Signer.String())
 	if version.GTE(semver.MustParse("0.1.0")) {
-		return h.handleV1(ctx, msg)
+		return h.handleV1(ctx, msg, constAccessor)
 	} else {
 		ctx.Logger().Error(badVersion.Error())
 		return errBadVersion.Result()
 	}
 }
 
-func (h SwapHandler) handleV1(ctx sdk.Context, msg MsgSwap) sdk.Result {
-	amount, err := swap(
+func (h SwapHandler) addSwapEvent(ctx sdk.Context, swapEvt EventSwap, tx common.Tx, status EventStatus) error {
+	swapBytes, err := json.Marshal(swapEvt)
+	if err != nil {
+		return err
+	}
+	evt := NewEvent(swapEvt.Type(), ctx.BlockHeight(), tx, swapBytes, status)
+	if err := h.keeper.UpsertEvent(ctx, evt); nil != err {
+		return err
+	}
+	return nil
+}
+
+func (h SwapHandler) handleV1(ctx sdk.Context, msg MsgSwap, constAccessor constants.ConstantValues) sdk.Result {
+	transactionFee := constAccessor.GetInt64Value(constants.TransactionFee)
+	amount, swapEvents, err := swap(
 		ctx,
 		h.keeper,
 		msg.Tx,
 		msg.TargetAsset,
 		msg.Destination,
 		msg.TradeTarget,
-	)
+		sdk.NewUint(uint64(transactionFee)))
 	if err != nil {
 		ctx.Logger().Error("fail to process swap message", "error", err)
 		return sdk.ErrInternal(err.Error()).Result()
+	}
+	for _, item := range swapEvents {
+		if eventErr := h.addSwapEvent(ctx, item, msg.Tx, EventPending); eventErr != nil {
+			return sdk.ErrInternal(eventErr.Error()).Result()
+		}
 	}
 
 	res, err := h.keeper.Cdc().MarshalBinaryLengthPrefixed(
