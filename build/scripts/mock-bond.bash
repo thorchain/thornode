4549907c750dca4b18b8804a650b071cae13eb87
Son of Odin <dev@thorchain.org>
2020-01-07 14:57:48 +0800

    run mock bond

--- build/scripts/mock-bond.bash
+++ build/scripts/mock-bond.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-# ./mock-bond.bash <mock binance IP address> <thor/node address>
-# ./mock-bond.bash 127.0.0.1 thor1kljxxccrheghavaw97u78le6yy3sdj7h696nl4
+# ./mock-bond.bash <mock binance IP address> <BNB Address> <thor/node address>
+# ./mock-bond.bash 127.0.0.1 bnbXYXYX thor1kljxxccrheghavaw97u78le6yy3sdj7h696nl4
 
 # set -ex
 
@@ -10,19 +10,22 @@ if [ -z $1 ]; then
 fi
 
 if [ -z $2 ]; then
+    echo "Missing bnb address argument"
+    exit 1
+fi
+
+if [ -z $3 ]; then
     echo "Missing node address argument (thor address)"
     exit 1
 fi
 
 POOL_ADDRESS=$(curl -s $1:1317/thorchain/pool_addresses | jq -r ".current[0].address")
 
-# NOTE: the from address doesn't matter at all (mock binance doesn't care)
-
 curl -vvv -s -X POST -d "{
-  \"from\": \"tbnb1rlmrd83gv7rk2thusqm7dx38z8jgur80t8kq28\",
+  \"from\": \"$2\",
   \"to\": \"$POOL_ADDRESS\",
   \"coins\":[
       {\"denom\": \"RUNE-A1F\", \"amount\": 100000000000000}
   ],
-  \"memo\": \"BOND:$2\"
+  \"memo\": \"BOND:$3\"
 }" $1:26660/broadcast/easy
