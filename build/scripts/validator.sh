fa93e5b0fc5d488f7cf903f84987d6d935cedd32
Son of Odin <dev@thorchain.org>
2020-01-08 16:35:08 +0800

    add a wait

--- build/scripts/validator.sh
+++ build/scripts/validator.sh
@@ -36,7 +36,7 @@ if [ ! -f ~/.thord/config/genesis.json ]; then
         # send bond transaction to mock binance
         $(dirname "$0")/mock-bond.sh $PEER $ADDRESS $NODE_ADDRESS
 
-        sleep 5 # wait for thorchain to register the new node account
+        sleep 10 # wait for thorchain to register the new node account
 
         # set node keys
         echo $SIGNER_PASSWD | thorcli tx thorchain set-node-keys $(thorcli keys show thorchain --pubkey) $(thorcli keys show thorchain --pubkey) $(thord tendermint show-validator) --node tcp://$PEER:26657 --from $SIGNER_NAME --yes
