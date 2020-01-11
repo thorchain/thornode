bbb42e82f8e7167f94bc348e1ba2befdb64ddb6b
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-01-11 18:10:29 +0800

    update script

--- build/linux/ubuntu/docker.bash
+++ build/linux/ubuntu/docker.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+BRANCH=${VARIABLE:=master}
+
 apt install make
 
 git clone https://gitlab.com/thorchain/thornode.git
 cd thornode
-git checkout validator-run
+git checkout $BRANCH
 
-PEER=67.205.172.201 BINANCE_HOST=http://67.205.172.201:26660 TAG=sandbox TAGS=sandbox make -C build/docker reset-mocknet-validator
+PEER=$PEER BINANCE_HOST=$BINANCE_HOST make -C build/docker reset-mocknet-validator
