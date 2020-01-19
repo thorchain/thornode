c683e8f90e474c21c79106e8aec7c56cad13eb04
Son of Odin <dev@thorchain.org>
2020-01-19 12:36:16 +1100

    tweaks

--- constants/constants_sandbox.go
+++ constants/constants_sandbox.go
@@ -5,11 +5,12 @@ package constants
 
 func init() {
 	int64Overrides = map[ConstantName]int64{
-		DesireValidatorSet:   12,
-		RotatePerBlockHeight: 60,          // 5 min
-		BadValidatorRate:     60,          // 5 min
-		OldValidatorRate:     60,          // 5 min
-		MinimumBondInRune:    100_000_000, // 1 rune
+		DesireValidatorSet:    12,
+		RotatePerBlockHeight:  60,          // 5 min
+		BadValidatorRate:      60,          // 5 min
+		OldValidatorRate:      60,          // 5 min
+		MinimumBondInRune:     100_000_000, // 1 rune
+		FundMigrationInterval: 10,
 	}
 	boolOverrides = map[ConstantName]bool{
 		StrictBondStakeRatio: false,
