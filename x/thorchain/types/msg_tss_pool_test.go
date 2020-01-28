e6af7e4ccd7fdd72a39973827cd43d7e8652d81c
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-01-28 11:11:54 -0500

    collect blame from bifrost

--- x/thorchain/types/msg_tss_pool_test.go
+++ x/thorchain/types/msg_tss_pool_test.go
@@ -15,12 +15,12 @@ func (s *MsgTssPoolSuite) TestMsgTssPool(c *C) {
 		GetRandomPubKey(), GetRandomPubKey(), GetRandomPubKey(),
 	}
 	addr := GetRandomBech32Addr()
-	msg := NewMsgTssPool(pks, pk, addr)
+	msg := NewMsgTssPool(pks, pk, common.EmptyBlame, addr)
 	c.Check(msg.Type(), Equals, "set_tss_pool")
 	c.Assert(msg.ValidateBasic(), IsNil)
 
-	c.Check(NewMsgTssPool(pks, pk, nil).ValidateBasic(), NotNil)
-	c.Check(NewMsgTssPool(nil, pk, addr).ValidateBasic(), NotNil)
-	c.Check(NewMsgTssPool(pks, "", addr).ValidateBasic(), NotNil)
-	c.Check(NewMsgTssPool(pks, "bogusPubkey", addr).ValidateBasic(), NotNil)
+	c.Check(NewMsgTssPool(pks, pk, common.EmptyBlame, nil).ValidateBasic(), NotNil)
+	c.Check(NewMsgTssPool(nil, pk, common.EmptyBlame, addr).ValidateBasic(), NotNil)
+	c.Check(NewMsgTssPool(pks, "", common.EmptyBlame, addr).ValidateBasic(), NotNil)
+	c.Check(NewMsgTssPool(pks, "bogusPubkey", common.EmptyBlame, addr).ValidateBasic(), NotNil)
 }
