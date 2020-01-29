5c6aa01e439f97a6d05c6d922dbc53a9d23b98df
Heimdall <heimdallthor02@gmail.com>
2020-01-29 21:38:46 +0000

    330-issue if node account sending out fund using `yggdrasil-` , however the to address is not active asgard, slash them

--- x/thorchain/handler_yggdrasil.go
+++ x/thorchain/handler_yggdrasil.go
@@ -2,6 +2,7 @@ package thorchain
 
 import (
 	stdErrors "errors"
+	"fmt"
 
 	"github.com/blang/semver"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -10,6 +11,12 @@ import (
 )
 
 // YggdrasilHandler is to process yggdrasil messages
+// When thorchain fund yggdrasil pool , observer should observe two transactions
+// 1. outbound tx from asgard vault
+// 2. inbound tx to yggdrasil vault
+// when yggdrasil pool return fund , observer should observe two transactions as well
+// 1. outbound tx from yggdrasil vault
+// 2. inbound tx to asgard vault
 type YggdrasilHandler struct {
 	keeper       Keeper
 	txOutStore   VersionedTxOutStore
@@ -32,29 +39,27 @@ func (h YggdrasilHandler) Run(ctx sdk.Context, m sdk.Msg, version semver.Version
 		return errInvalidMessage.Result()
 	}
 	if err := h.validate(ctx, msg, version); err != nil {
-		return sdk.ErrInternal(err.Error()).Result()
+		return err.Result()
 	}
 	return h.handle(ctx, msg, version, constAccessor)
 }
 
-func (h YggdrasilHandler) validate(ctx sdk.Context, msg MsgYggdrasil, version semver.Version) error {
+func (h YggdrasilHandler) validate(ctx sdk.Context, msg MsgYggdrasil, version semver.Version) sdk.Error {
 	if version.GTE(semver.MustParse("0.1.0")) {
 		return h.validateV1(ctx, msg)
-	} else {
-		ctx.Logger().Error(errInvalidVersion.Error())
-		return errInvalidVersion
 	}
+	ctx.Logger().Error(errInvalidVersion.Error())
+	return errBadVersion
 }
 
-func (h YggdrasilHandler) validateV1(ctx sdk.Context, msg MsgYggdrasil) error {
+func (h YggdrasilHandler) validateV1(ctx sdk.Context, msg MsgYggdrasil) sdk.Error {
 	if err := msg.ValidateBasic(); nil != err {
 		ctx.Logger().Error(err.Error())
 		return err
 	}
 
 	if !isSignedByActiveNodeAccounts(ctx, h.keeper, msg.GetSigners()) {
-		ctx.Logger().Error(notAuthorized.Error())
-		return notAuthorized
+		return sdk.ErrUnauthorized("not signed by an active node account")
 	}
 	return nil
 }
@@ -70,65 +75,108 @@ func (h YggdrasilHandler) handle(ctx sdk.Context, msg MsgYggdrasil, version semv
 }
 
 func (h YggdrasilHandler) handleV1(ctx sdk.Context, msg MsgYggdrasil, version semver.Version) sdk.Result {
-	ygg, err := h.keeper.GetVault(ctx, msg.PubKey)
+	vault, err := h.keeper.GetVault(ctx, msg.PubKey)
 	if nil != err && !stdErrors.Is(err, ErrVaultNotFound) {
 		ctx.Logger().Error("fail to get yggdrasil", "error", err)
 		return sdk.ErrInternal(err.Error()).Result()
 	}
-	if len(ygg.Type) == 0 {
-		ygg.Status = ActiveVault
-		ygg.Type = YggdrasilVault
+	if len(vault.Type) == 0 {
+		vault.Status = ActiveVault
+		vault.Type = YggdrasilVault
 	}
 
-	if !ygg.IsYggdrasil() {
-		return sdk.ErrInternal("this is not a Yggdrasil vault").Result()
-	}
 	if msg.AddFunds {
+		return h.handleYggdrasilFund(ctx, msg, vault)
+	}
+	return h.handleYggdrasilReturn(ctx, msg, vault, version)
+}
+
+func (h YggdrasilHandler) handleYggdrasilFund(ctx sdk.Context, msg MsgYggdrasil, vault Vault) sdk.Result {
+	if vault.Type == AsgardVault {
 		ctx.EventManager().EmitEvent(
-			sdk.NewEvent("yggdrasil_fund",
-				sdk.NewAttribute("pubkey", ygg.PubKey.String()),
+			sdk.NewEvent("asgard_fund_yggdrasil",
+				sdk.NewAttribute("pubkey", vault.PubKey.String()),
+				sdk.NewAttribute("coins", msg.Coins.String()),
+				sdk.NewAttribute("tx", msg.Tx.ID.String())))
+	}
+	if vault.Type == YggdrasilVault {
+		ctx.EventManager().EmitEvent(
+			sdk.NewEvent("yggdrasil_receive_fund",
+				sdk.NewAttribute("pubkey", vault.PubKey.String()),
+				sdk.NewAttribute("coins", msg.Coins.String()),
+				sdk.NewAttribute("tx", msg.Tx.ID.String())))
+	}
+	// Yggdrasil usually comes from Asgard , Asgard --> Yggdrasil
+	// It will be an outbound tx from Asgard pool , and it will be an Inbound tx form Yggdrasil pool
+	// incoming fund will be added to Vault as part of ObservedTxInHandler
+	// Yggdrasil handler doesn't need to do anything
+	return sdk.Result{
+		Code:      sdk.CodeOK,
+		Codespace: DefaultCodespace,
+	}
+}
+
+func (h YggdrasilHandler) handleYggdrasilReturn(ctx sdk.Context, msg MsgYggdrasil, vault Vault, version semver.Version) sdk.Result {
+	// observe an outbound tx from yggdrasil vault
+	if vault.Type == YggdrasilVault {
+		asgardVaults, err := h.keeper.GetAsgardVaultsByStatus(ctx, ActiveVault)
+		if nil != err {
+			ctx.Logger().Error("unable to get asgard vaults", "error", err)
+			return sdk.ErrInternal("unable to get asgard vaults").Result()
+		}
+		isAsgardReceipient, err := asgardVaults.HasAddress(msg.Tx.Chain, msg.Tx.ToAddress)
+		if nil != err {
+			ctx.Logger().Error(fmt.Sprintf("unable to determinate whether %s is an Asgard vault", msg.Tx.ToAddress), "error", err)
+			return sdk.ErrInternal("unable to check recipient against active Asgards").Result()
+		}
+
+		if !isAsgardReceipient {
+			// not sending to asgard , slash the node account
+			for _, c := range msg.Tx.Coins {
+				if err := slashNodeAccount(ctx, h.keeper, msg.PubKey, c.Asset, c.Amount); nil != err {
+					ctx.Logger().Error("fail to slash account for sending fund to a none asgard vault using yggdrasil-", "error", err)
+					return sdk.ErrInternal("fail to slash account").Result()
+				}
+			}
+		}
+
+		na, err := h.keeper.GetNodeAccountByPubKey(ctx, msg.PubKey)
+		if err != nil {
+			ctx.Logger().Error("unable to get node account", "error", err)
+			return sdk.ErrInternal(err.Error()).Result()
+		}
+		if na.Status == NodeActive {
+			// node still active , no refund bond
+			return sdk.Result{
+				Code:      sdk.CodeOK,
+				Codespace: DefaultCodespace,
+			}
+		}
+		txOutStore, err := h.txOutStore.GetTxOutStore(h.keeper, version)
+		if nil != err {
+			ctx.Logger().Error("fail to get txout store", "error", err)
+			return errBadVersion.Result()
+		}
+		if err := refundBond(ctx, msg.Tx, na, h.keeper, txOutStore); err != nil {
+			ctx.Logger().Error("fail to refund bond", "error", err)
+			return sdk.ErrInternal(err.Error()).Result()
+		}
+		return sdk.Result{
+			Code:      sdk.CodeOK,
+			Codespace: DefaultCodespace,
+		}
+	}
+
+	// when vault.Type is asgard, that means this tx is observed on an asgard pool and it is an inbound tx
+	if vault.Type == AsgardVault {
+		// Yggdrasil return fund back to Asgard
+		ctx.EventManager().EmitEvent(
+			sdk.NewEvent("yggdrasil_return",
+				sdk.NewAttribute("pubkey", vault.PubKey.String()),
 				sdk.NewAttribute("coins", msg.Coins.String()),
 				sdk.NewAttribute("tx", msg.Tx.ID.String())))
 
-		// Yggdrasil usually comes from Asgard , Asgard --> Yggdrasil
-		// It will be an outbound tx from Asgard pool , and it will be an Inbound tx form Yggdrasil pool
-		// incoming fund will be added to Vault as part of ObservedTxInHandler
-		// Yggdrasil handler doesn't need to do anything
-		return sdk.Result{
-			Code:      sdk.CodeOK,
-			Codespace: DefaultCodespace,
-		}
 	}
-
-	// Yggdrasil return fund back to Asgard
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent("yggdrasil_return",
-			sdk.NewAttribute("pubkey", ygg.PubKey.String()),
-			sdk.NewAttribute("coins", msg.Coins.String()),
-			sdk.NewAttribute("tx", msg.Tx.ID.String())))
-
-	na, err := h.keeper.GetNodeAccountByPubKey(ctx, msg.PubKey)
-	if err != nil {
-		ctx.Logger().Error("unable to get node account", "error", err)
-		return sdk.ErrInternal(err.Error()).Result()
-	}
-	if na.Status == NodeActive {
-		// node still active , no refund bond
-		return sdk.Result{
-			Code:      sdk.CodeOK,
-			Codespace: DefaultCodespace,
-		}
-	}
-	txOutStore, err := h.txOutStore.GetTxOutStore(h.keeper, version)
-	if nil != err {
-		ctx.Logger().Error("fail to get txout store", "error", err)
-		return errBadVersion.Result()
-	}
-	if err := refundBond(ctx, msg.Tx, na, h.keeper, txOutStore); err != nil {
-		ctx.Logger().Error("fail to refund bond", "error", err)
-		return sdk.ErrInternal(err.Error()).Result()
-	}
-
 	return sdk.Result{
 		Code:      sdk.CodeOK,
 		Codespace: DefaultCodespace,
