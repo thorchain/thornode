b26bfac8ffc57ebda4490ec483600fef8e107b71
Son of Odin <dev@thorchain.org>
2020-02-01 13:10:36 -0500

    tweaks

--- common/blame.go
+++ common/blame.go
@@ -5,10 +5,7 @@ import (
 	"strings"
 )
 
-var EmptyBlame = Blame{
-	FailReason: "",
-	BlameNodes: make(PubKeys, 0),
-}
+var EmptyBlame = Blame{}
 
 type Blame struct {
 	FailReason string  `json:"fail_reason"`
@@ -22,6 +19,6 @@ func (b Blame) IsEmpty() bool {
 func (b Blame) String() string {
 	sb := strings.Builder{}
 	sb.WriteString("reason:" + b.FailReason + "\n")
-	sb.WriteString("nodes:" + fmt.Sprintf("%+v", b.BlameNodes) + "\n")
+	sb.WriteString(fmt.Sprintf("nodes:%+v\n", b.BlameNodes))
 	return sb.String()
 }
