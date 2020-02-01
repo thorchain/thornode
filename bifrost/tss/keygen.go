8bcfa8bf030f6c04b73e5375b36ec1705a0e004b
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-02-01 12:21:06 -0500

    try omitting json

--- bifrost/tss/keygen.go
+++ bifrost/tss/keygen.go
@@ -77,6 +77,9 @@ func (kg *KeyGen) GenerateNewKey(pKeys common.PubKeys) (common.PubKeySet, common
 	if err != nil {
 		return common.EmptyPubKeySet, common.EmptyBlame, fmt.Errorf("fail to unmarshal tss keygen response,err:%w", err)
 	}
+	if len(dat.Blame.BlameNodes) == 0 {
+		dat.Blame.BlameNodes = nil
+	}
 
 	cpk, err := common.NewPubKey(dat.PubKey)
 	if err != nil {
