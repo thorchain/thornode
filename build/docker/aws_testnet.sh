53e5c3f274e289e70a9776be156596598c77b532
Fulgur Cete <itiboribo@yahoo.com>
2020-02-05 00:42:46 +0000

    ensure daily automated churning

--- build/docker/aws_testnet.sh
+++ build/docker/aws_testnet.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-USER=$(hostname)
-DISK_SIZE=100
-cd ../../
-LOCAL_VOLUME=$(pwd)
-
-if [ $1 == "churn" ]; then
-    export CHURN="true"
-fi
-
-###########
-# CLEANUP #
-###########
 cleanup () {
     echo "performing cleanup"
     if [ ! -z "${CI}" ]; then
@@ -30,7 +18,7 @@ which docker-machine
 if [ $? != 0 ]; then
     echo "installing docker-machine"
     base=https://github.com/docker/machine/releases/download/v0.16.2
-    curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&  install /tmp/docker-machine /usr/local/bin/docker-machine
+    curl -sL $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&  install /tmp/docker-machine /usr/local/bin/docker-machine
 fi
 
 which docker-compose
@@ -43,46 +31,84 @@ fi
 ####################
 # START THE STACK  #
 ####################
+create_server() {
+    cleanup ${DOCKER_SERVER} 20
+    cd ../../
+    LOCAL_VOLUME=$(pwd)
+    if [ ! -z "${CI}" ]; then
+        aws s3 cp s3://${BUCKET_NAME}/$SSH_PUB_KEY /tmp/.
+        aws s3 cp s3://${BUCKET_NAME}/$SSH_PRIV_KEY /tmp/.
+        chmod 0600 /tmp/$SSH_PRIV_KEY
+	    echo "creating server node on AWS"
+	    docker-machine create --driver amazonec2 \
+            --amazonec2-vpc-id=${AWS_VPC_ID} \
+            --amazonec2-region ${AWS_REGION} \
+            --amazonec2-instance-type ${AWS_INSTANCE_TYPE} \
+            --amazonec2-root-size ${DISK_SIZE} \
+            --amazonec2-ssh-keypath /tmp/$SSH_PRIV_KEY \
+            ${DOCKER_SERVER}
+    else
+        docker-machine create --driver amazonec2 \
+            --amazonec2-vpc-id=${AWS_VPC_ID} \
+            --amazonec2-region ${AWS_REGION} \
+            --amazonec2-instance-type ${AWS_INSTANCE_TYPE} \
+            --amazonec2-root-size ${DISK_SIZE} \
+            ${DOCKER_SERVER}
+    fi
+    if [ $? != 0 ]; then
+        echo "server could not be created"
+        exit 1
+    fi
+    echo "mounting volumes"
+    docker-machine ssh ${DOCKER_SERVER} sudo mkdir -p ${LOCAL_VOLUME}
+    docker-machine ssh ${DOCKER_SERVER} sudo chmod 777 -R ${LOCAL_VOLUME}
+    docker-machine scp -r ${LOCAL_VOLUME}/. ${DOCKER_SERVER}:${LOCAL_VOLUME}
+}
+
 start_the_stack () {
     cd build/docker/${THORNODE_ENV}
     echo "waiting for server to be ready"
     PROVISIONING_TIME=$1
     sleep ${PROVISIONING_TIME}
     export NET=${THORNODE_ENV}
-
-    if [ "$THORNODE_ENV" = "mocknet" ]; then
-        export TAG=sandbox
-    else
-        export TAG=${THORNODE_ENV}
-    fi
+    export TAG=${THORNODE_ENV}
     eval $(docker-machine env ${DOCKER_SERVER} --shell bash)
-    if [ ! -z "${CHURN}" ]; then
-        PEER=$(docker-machine ip $USER-$THORNODE_ENV)
-        export PEER && make run-${THORNODE_ENV}-validator
-        sleep 60
-        export FAUCET_PASSWORD=$FAUCET_PASSWORD && . ./../../scripts/make-testnet-bond.sh
-    elif [ ! -z "${CI}" ]; then
-        make reset-${THORNODE_ENV}-genesis-ci
+    PEER=$(curl -s http://thorchain.net.s3-website-us-east-1.amazonaws.com/net/)
+    if [ ! -z "${CI}" ]; then
+        export PEER=$PEER && make run-${THORNODE_ENV}-validator-ci
+    elif [ ! -z "${NON_CI}" ]; then
+        export PEER=$PEER && make run-${THORNODE_ENV}-validator
     else
         make run-${THORNODE_ENV}-standalone
     fi
     sleep 60
 }
 
+##################
+# CHURN
+##################
+churn () {
+    export FAUCET_PASSWORD=$FAUCET_PASSWORD && . ./../../scripts/make-testnet-bond.sh
+    #################################################################
+    # wait for bond transaction and for node account to be registered
+    #################################################################
+    eval $(docker-machine env ${DOCKER_SERVER} --shell bash)
+    PUB_KEY=$(docker exec thor-daemon thorcli keys show thorchain --pubkey)
+    VALIDATOR=$(docker exec thor-daemon thord tendermint show-validator)
+    docker exec thor-daemon ash -c \
+        "echo $SIGNER_PASSWD | thorcli tx thorchain set-node-keys $PUB_KEY $PUB_KEY $VALIDATOR \
+        --node tcp://$PEER:26657 --from $SIGNER_NAME --yes"
+}
+
 #####################
 # VERIFY THE STACK  #
 #####################
 verify_the_stack () {
-    if [ ! -z "${CI}" ]; then
-        echo "no need to unset docker variables"
-    else
-        eval $(docker-machine env -u)
-    fi
-    docker-machine ssh ${DOCKER_SERVER} sudo docker ps -a
     echo "allow a few mins for docker services to come up"
-    sleep 120
+    sleep 60
+    docker ps -a
     echo "performing healthchecks"
-    IP=$(docker-machine ip ${DOCKER_SERVER})
+    export IP=$(docker-machine ip ${DOCKER_SERVER})
     HEALTHCHECK_URL="http://${IP}:8080/v1/thorchain/pool_addresses"
     HEALTHCHECK_CMD=$(curl -s -o /dev/null -w "%{http_code}" ${HEALTHCHECK_URL})
     if  [ "${HEALTHCHECK_CMD}" == 200 ]; then
@@ -91,39 +117,58 @@ verify_the_stack () {
 	    echo "HEALTHCHECK FAILED"
 	    exit 1
     fi
+    if [ ! -z "${CHURN}" ]; then
+        echo "starting churning"
+        churn
+    else
+        update_ip
+    fi
 }
 
-########################
-# CREATE DOCKER SERVER #
-########################
-if [ -z "${THORNODE_ENV}" ]; then
-    echo "please provide \$THORNODE_ENV environment variable"
-    exit 1
+################################
+# Register IP on S3 Web Endpoint
+################################
+update_ip() {
+    echo $IP > /tmp/ip_address
+    sed -e 's/"//g' -e "s/null//g" /tmp/ip_address > /tmp/${S3_FILE}
+    aws s3 cp /tmp/${S3_FILE} s3://${BUCKET_NAME}/net/
+}
+
+#########
+# START #
+#########
+THORNODE_ENV=testnet
+BUCKET_NAME="thorchain.net"
+S3_FILE="${THORNODE_ENV}.json"
+USER=computer
+DISK_SIZE=100
+SSH_PUB_KEY=gitlab-ci.pub
+SSH_PRIV_KEY=gitlab-ci
+
+if [ $1 == "ci" ]; then
+    export CI="true"
+    export AWS_REGION=$AWS_CI_REGION
+    export USER=$CI_JOB_ID
+    export CHURN="true"
+elif [ $1 == "churn" ]; then
+    export CHURN="true"
+    export NON_CI="true"
 fi
 
 export DOCKER_SERVER="${USER}-${THORNODE_ENV}$1"
 if [ ! -z "${AWS_VPC_ID}" ] && [ ! -z "${AWS_REGION}" ] && [ ! -z "${AWS_INSTANCE_TYPE}" ]; then
     cleanup ${DOCKER_SERVER} 20
-	echo "creating server on AWS"
-	docker-machine create --driver amazonec2 \
-        --amazonec2-vpc-id=${AWS_VPC_ID} \
-        --amazonec2-region ${AWS_REGION} \
-        --amazonec2-instance-type ${AWS_INSTANCE_TYPE} \
-        --amazonec2-root-size ${DISK_SIZE} \
-        ${DOCKER_SERVER}
-    if [ $? != 0 ]; then
-        echo "server could not be created"
-        exit 1
-    fi
-    ##################
-    # MOUNT VOLUMES ##
-    ##################
-    docker-machine ssh ${DOCKER_SERVER} sudo mkdir -p ${LOCAL_VOLUME}
-    docker-machine ssh ${DOCKER_SERVER} sudo chmod 777 -R ${LOCAL_VOLUME}
-    docker-machine scp -r ${LOCAL_VOLUME}/. ${DOCKER_SERVER}:${LOCAL_VOLUME}
+    create_server
     start_the_stack 60
     verify_the_stack
 else
 	echo "you have not provided all the required environment variables"
 	exit 1
+fi
+
+if [ ! -z "${CI}" ]; then
+    echo "no need to unset docker variables"
+    rm -rf /tmp/gitlab-ci*
+else
+    eval $(docker-machine env -u)
 fi
