c1ca07ea202d4f1e412d8cfc161a2375a2bf0929
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-02-05 21:40:57 -0500

    refactor signer to get txns from store

--- bifrost/signer/sign.go
+++ bifrost/signer/sign.go
@@ -165,6 +165,7 @@ func (s *Signer) signTransactions() {
 
 	for {
 		sleep()
+		items := s.storage.List()
 
 		for _, item := range s.storage.List() {
 			if item.Status == TxSpent { // don't rebroadcast spent transactions
