2cc3591ea0e3f14df2d744ec9f5c9ccbdba44fb9
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-02-06 10:34:54 -0500

    cleanup

--- bifrost/signer/storage_test.go
+++ bifrost/signer/storage_test.go
@@ -1,7 +1,6 @@
 package signer
 
 import (
-	"fmt"
 	"os"
 
 	. "gopkg.in/check.v1"
@@ -19,6 +18,11 @@ func (s *StorageSuite) TestStorage(c *C) {
 
 	store, err := NewSignerStore(dir)
 	c.Assert(err, IsNil)
+
+	// clean out any items that may already exist
+	for _, item := range store.List() {
+		c.Assert(store.Remove(item), IsNil)
+	}
 
 	item := NewTxOutStoreItem(12, types.TxOutItem{Memo: "foo"})
 
