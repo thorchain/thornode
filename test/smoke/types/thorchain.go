625ff0966e169c371ba5f06eaebc04b920c62509
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-02-07 15:32:52 -0500

    fix smoke tests

--- test/smoke/types/thorchain.go
+++ test/smoke/types/thorchain.go
@@ -16,26 +16,18 @@ type ThorchainPoolAddress struct {
 }
 
 type ThorchainPools []struct {
-	BalanceRune  int64 `json:"balance_rune,string"`
-	BalanceAsset int64 `json:"balance_asset,string"`
-	Asset        struct {
-		Chain  string `json:"chain"`
-		Symbol string `json:"symbol"`
-		Ticker string `json:"string"`
-	} `json:"asset"`
-	PoolUnits   int64  `json:"pool_units,string"`
-	PoolAddress string `json:"pool_address"`
-	Status      string `json:"status"`
+	BalanceRune  int64        `json:"balance_rune,string"`
+	BalanceAsset int64        `json:"balance_asset,string"`
+	Asset        common.Asset `json:"asset"`
+	PoolUnits    int64        `json:"pool_units,string"`
+	PoolAddress  string       `json:"pool_address"`
+	Status       string       `json:"status"`
 }
 
 type Staker struct {
 	StakerID     string `json:"staker_id"`
 	PoolAndUnits []struct {
-		Asset struct {
-			Chain  string `json:"chain"`
-			Symbol string `json:"symbol"`
-			Ticker string `json:"string"`
-		} `json:"asset"`
-		Units int64 `json:"units,string"`
+		Asset common.Asset `json:"asset"`
+		Units int64        `json:"units,string"`
 	} `json:"pool_and_units"`
 }
