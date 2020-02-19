7d4a600e0c5d748f9acc0c64229dd01fc7c0f135
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-02-19 13:52:57 -0500

    fix integration tests

--- test/integration/spec/api_spec.rb
+++ test/integration/spec/api_spec.rb
@@ -148,7 +148,7 @@ describe "API Tests" do
       # pool balance should not change (other than the transaction fees)
       resp = get("/pool/BNB.BOLT-014")
       expect(resp.code).to eq("200")
-      expect(resp.body['balance_rune']).to eq("2217077851"), resp.body.inspect
+      expect(resp.body['balance_rune']).to eq("2117077851"), resp.body.inspect
       expect(resp.body['balance_asset']).to eq("370801602"), resp.body.inspect
     end
 
