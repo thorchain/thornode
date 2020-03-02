9b8daaf6ef78ea50f3470266e091d547f5ab3288
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-03-02 10:25:08 -0500

    add comment

--- bifrost/pkg/chainclients/binance/binance.go
+++ bifrost/pkg/chainclients/binance/binance.go
@@ -404,6 +404,7 @@ func (b *Binance) signWithRetry(inHash common.TxID, signMsg btx.StdSignMsg, from
 		}
 
 		// if our event is already completed, don't bother trying to sign again
+		// TODO: this may be too slow of a mechanism for chains with longer block times
 		if evt.Status != tttypes.Pending {
 			b.logger.Debug().Msgf("Message already signed or failed. Skipping...")
 			return nil, nil
