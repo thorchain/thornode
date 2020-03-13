e024251ece8f50702e0b0b53ca44cb9169605b86
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-03-13 14:40:33 +0330

    Revert "Add rune to gas event"
    This reverts commit bbc46179f39fbca0293492ed15a8b9ab76cc4fd0.

--- x/thorchain/helpers.go
+++ x/thorchain/helpers.go
@@ -423,7 +423,6 @@ func AddGasFees(ctx sdk.Context, keeper Keeper, tx ObservedTx) error {
 		return err
 	}
 
-	var runeGas common.Gas
 	// Subtract gas from pools (will be reimbursed later with rune at the end
 	// of the block)
 	for _, gas := range tx.Tx.Gas {
@@ -433,7 +432,6 @@ func AddGasFees(ctx sdk.Context, keeper Keeper, tx ObservedTx) error {
 		}
 		pool.Asset = gas.Asset
 		pool.BalanceAsset = common.SafeSub(pool.BalanceAsset, gas.Amount)
-		runeGas = append(runeGas, common.Coin{common.RuneAsset(), pool.AssetValueInRune(gas.Amount)})
 		if err := keeper.SetPool(ctx, pool); err != nil {
 			return err
 		}
@@ -451,8 +449,7 @@ func AddGasFees(ctx sdk.Context, keeper Keeper, tx ObservedTx) error {
 			return err
 		}
 	}
-	totalGas := append(tx.Tx.Gas, runeGas...)
-	eventGas := NewEventGas(totalGas, GasSpend)
+	eventGas := NewEventGas(tx.Tx.Gas, GasSpend)
 	gasBuf, err := json.Marshal(eventGas)
 	if err != nil {
 		return fmt.Errorf("fail to marshal gas event to buf: %w", err)
