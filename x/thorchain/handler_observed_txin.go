7800e323598349f1f67e4ac32ee1e06178b45bad
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-03-29 15:52:40 -0400

    [bugfix] observe addrs after txn is processed

--- x/thorchain/handler_observed_txin.go
+++ x/thorchain/handler_observed_txin.go
@@ -153,6 +153,14 @@ func (h ObservedTxInHandler) handleV1(ctx sdk.Context, version semver.Version, m
 
 		voter, ok := h.preflight(ctx, voter, activeNodeAccounts, tx, msg.Signer)
 		if !ok {
+			if voter.Height > 0 {
+				// we've already process the transaction, but we should still
+				// update the observing addresses
+				txIn := voter.GetTx(activeNodeAccounts)
+				if err := h.keeper.AddObservingAddresses(ctx, txIn.Signers); err != nil {
+					ctx.Logger().Error("fail to add observing address", "error", err)
+				}
+			}
 			continue
 		}
 		ctx.Logger().Info("handleMsgObservedTxIn request", "Tx:", msg.Txs[0].String())
