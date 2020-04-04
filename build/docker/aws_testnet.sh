8bee9430b5a47b6a6ddef01b652674d341b3c1a3
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-04-04 15:21:54 -0400

    update hostname

--- build/docker/aws_testnet.sh
+++ build/docker/aws_testnet.sh
@@ -63,8 +63,7 @@ create_server() {
         aws s3 cp s3://${BUCKET_NAME}/$SSH_PRIV_KEY /tmp/.
         chmod 0600 /tmp/$SSH_PRIV_KEY
 	    echo "creating server node on AWS"
-        export MACHINE_NAME="$(hostname)-$(date +%s)"
-        echo $MACHINE_NAME
+        export MACHINE_NAME="$(hostname)-${THORNODE_SERVICE}-$(date +%s)"
 	    docker-machine create --driver amazonec2 \
             --amazonec2-vpc-id=${AWS_VPC_ID} \
             --amazonec2-region ${AWS_REGION} \
