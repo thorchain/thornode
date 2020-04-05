eb672f5ebe813b6119e678ab29e469e99c73a638
skyorion427 <skyorion427@gmail.com>
2020-04-05 02:10:05 -0400

    Fix issues in the lambda function

--- build/lambda/node_ip_list_update/lambda_function.py
+++ build/lambda/node_ip_list_update/lambda_function.py
@@ -2,41 +2,42 @@ import json
 import requests
 import boto3
 
+prefix = 'node_ip_'
+s3_resource = boto3.resource('s3')
+buckets = ['testnet-seed.thorchain.info']
+
 def get_new_ip_list(ip_addr):
   response = requests.get('http://' + ip_addr + ':26657/net_info')
-  peers = response.json()['result']['peers']
+  peers = [x['remote_ip'] for x in response.json()['result']['peers']]
+  peers.append(ip_addr)
 
-  new_ip_list = []
-  for peer in peers:
-    remote_ip = peer['remote_ip']
-    new_ip_list.append(remote_ip)
+  return list(set(peers))
 
-  new_ip_list = list(set(new_ip_list)) # avoid duplicates
-
-  return new_ip_list
 
 def lambda_handler(event, context):
-  s3_resource = boto3.resource('s3')
-  bucket = 'testnet-seed.thorchain.info'
-  prefix = 'node_ip_'
-  thorchain_bucket = s3_resource.Bucket(bucket)
-
   try:
-    for obj in thorchain_bucket.objects.all():
-      key = obj.key
-      if (key[0:8] == prefix):
-        print('key: ' + key)
-        body = obj.get()['Body'].read()
-        ip_list = json.loads(body)
+    for bucket in buckets:
+      thorchain_bucket = s3_resource.Bucket(bucket)
 
-        updated_ip_list = []
-        for ip_addr in ip_list:
-          updated_ip_list += get_new_ip_list(ip_addr)
-        
+      for obj in thorchain_bucket.objects.all():
+        key = obj.key
+        if key.startsWith(prefix):
+          print('key: ' + key)
+          body = obj.get()['Body'].read()
+          ip_list = json.loads(body)
 
-        updated_ip_list = list(set(updated_ip_list)) # avoid duplicates
-        new_body = json.dumps(updated_ip_list)
-        obj.put(Body=new_body)
+          updated_ip_list = []
+          for ip_addr in ip_list:
+            updated_ip_list += get_new_ip_list(ip_addr)
+          
+
+          updated_ip_list = list(set(updated_ip_list)) # avoid duplicates
+          new_body = json.dumps(updated_ip_list)
+          obj.put(Body=new_body)
+
+          if len(updated_ip_list) == 0:
+            return {'message': 'no IP addresses found!'}
+
 
     return {
       'message': 'successfully updated!'
