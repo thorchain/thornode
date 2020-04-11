684388549041a7b92e8545771383a08c845f2c45
Heimdall Thor <heimdallthor02@gmail.com>
2020-04-11 13:37:35 +1000

    WIP

--- bifrost/pkg/chainclients/bitcoin/bitcoin_signer.go
+++ bifrost/pkg/chainclients/bitcoin/bitcoin_signer.go
@@ -5,13 +5,49 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/btcsuite/btcd/btcjson"
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 
 	stypes "gitlab.com/thorchain/thornode/bifrost/thorclient/types"
 	"gitlab.com/thorchain/thornode/common"
 )
+
+func (c *Client) getChainCfg() *chaincfg.Params {
+	cn := common.GetCurrentChainNetwork()
+	switch cn {
+	case common.TestNet:
+		return &chaincfg.TestNet3Params
+	case common.MainNet:
+		return &chaincfg.MainNetParams
+	}
+	return nil
+}
+func (c *Client) getAllUnspentUtxo() ([]string, error) {
+	return nil, nil
+}
+
+func (c *Client) getLastOutput(inputTxId, sourceAddr string) (btcjson.Vout, error) {
+	txHash, err := chainhash.NewHashFromStr(inputTxId)
+	if err != nil {
+		return btcjson.Vout{}, fmt.Errorf("fail to parse (%s) as chain hash: %w", inputTxId, err)
+	}
+	txRaw, err := c.client.GetRawTransactionVerbose(txHash)
+	if err != nil {
+		return btcjson.Vout{}, fmt.Errorf("fail to get the raw transactional: %w", err)
+	}
+	for _, item := range txRaw.Vout {
+		for _, addr := range item.ScriptPubKey.Addresses {
+			if addr == sourceAddr {
+				return item, nil
+			}
+		}
+	}
+	return btcjson.Vout{}, errors.New("not found")
+}
 
 // SignTx is going to generate the outbound transaction, and also sign it
 func (c *Client) SignTx(tx stypes.TxOutItem, height int64) ([]byte, error) {
@@ -23,13 +59,90 @@ func (c *Client) SignTx(tx stypes.TxOutItem, height int64) ([]byte, error) {
 		return nil, fmt.Errorf("fail to get source address: %w", err)
 	}
 
-	addr, err := btcutil.DecodeAddress(sourceAddr.String(), &chaincfg.TestNet3Params)
+	addr, err := btcutil.DecodeAddress(sourceAddr.String(), c.getChainCfg())
 	if err != nil {
 		return nil, fmt.Errorf("fail to decode source address(%s): %w", sourceAddr.String(), err)
 	}
-	_ = addr
-	// first of all get all the UTXO
-	return nil, nil
+	sourceScript, err := txscript.PayToAddrScript(addr)
+	if err != nil {
+		return nil, fmt.Errorf("fail to get source pay to address script: %w", err)
+	}
+	txes, err := c.getAllUnspentUtxo()
+	if err != nil {
+		return nil, fmt.Errorf("fail to get unspent UTXO")
+	}
+	redeemTx := wire.NewMsgTx(wire.TxVersion)
+	totalAmt := float64(0)
+	individualAmounts := make([]btcutil.Amount, len(txes))
+	for idx, item := range txes {
+		vOut, err := c.getLastOutput(item, sourceAddr.String())
+		if err != nil {
+			return nil, fmt.Errorf("sorry didn't find last output for tx(%s): %w", item, err)
+		}
+		c.logger.Info().Msgf("find last output, value: %f BTC,index: %d", vOut.Value, vOut.N)
+		txHash, err := chainhash.NewHashFromStr(item)
+		if err != nil {
+			return nil, fmt.Errorf("fail to parse txhash(%s): %w", item, err)
+		}
+		outputPoint := wire.NewOutPoint(txHash, vOut.N)
+		sourceTxIn := wire.NewTxIn(outputPoint, nil, nil)
+		redeemTx.AddTxIn(sourceTxIn)
+		totalAmt += vOut.Value
+		amt, err := btcutil.NewAmount(vOut.Value)
+		if err != nil {
+			return nil, fmt.Errorf("fail to parse amount(%f): %w", vOut.Value, err)
+		}
+		individualAmounts[idx] = amt
+	}
+
+	outputAddr, err := btcutil.DecodeAddress(tx.ToAddress.String(), c.getChainCfg())
+	if err != nil {
+		return nil, fmt.Errorf("fail to decode next address: %w", err)
+	}
+	buf, err := txscript.PayToAddrScript(outputAddr)
+	if err != nil {
+		return nil, fmt.Errorf("fail to get pay to address script: %w", err)
+	}
+
+	total, err := btcutil.NewAmount(totalAmt)
+	if err != nil {
+		return nil, fmt.Errorf("fail to parse total amount(%f),err: %w", totalAmt, err)
+	}
+
+	redeemTxOut := wire.NewTxOut(int64(total)-1501, buf)
+	redeemTx.AddTxOut(redeemTxOut)
+
+	nullDataScript, err := txscript.NullDataScript([]byte(tx.Memo))
+	if err != nil {
+		return nil, fmt.Errorf("fail to generate null data script: %w", err)
+	}
+	redeemTx.AddTxOut(wire.NewTxOut(0, nullDataScript))
+
+	for idx := range redeemTx.TxIn {
+		sigHashes := txscript.NewTxSigHashes(redeemTx)
+
+		witness, err := txscript.WitnessSignature(redeemTx, sigHashes, idx, int64(individualAmounts[idx]), sourceScript, txscript.SigHashAll, privateKey, true)
+		if err != nil {
+			return nil, fmt.Errorf("fail to get witness: %w", err)
+		}
+
+		redeemTx.TxIn[idx].Witness = witness
+		flag := txscript.StandardVerifyFlags
+		engine, err := txscript.NewEngine(sourceScript, redeemTx, idx, flag, nil, nil, int64(individualAmounts[idx]))
+		if err != nil {
+			return nil, fmt.Errorf("fail to create engine: %w", err)
+		}
+		if err := engine.Execute(); err != nil {
+			return nil, fmt.Errorf("fail to execute the script: %w", err)
+		}
+	}
+
+	var signedTx bytes.Buffer
+	if err := redeemTx.Serialize(&signedTx); err != nil {
+		return nil, fmt.Errorf("fail to serialize tx to bytes: %w", err)
+	}
+
+	return signedTx.Bytes(), nil
 }
 
 // BroadcastTx will broadcast the given payload to BTC chain
