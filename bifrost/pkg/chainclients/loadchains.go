1872f326a20b82b09e136ca3d7b5bd7302b80cdb
Fandral <fandral.dev@gmail.com>
2020-04-13 10:59:16 +1000

    Log error when load chain

--- bifrost/pkg/chainclients/loadchains.go
+++ bifrost/pkg/chainclients/loadchains.go
@@ -1,6 +1,7 @@
 package chainclients
 
 import (
+	"github.com/rs/zerolog/log"
 	"gitlab.com/thorchain/thornode/bifrost/config"
 	"gitlab.com/thorchain/thornode/bifrost/metrics"
 	"gitlab.com/thorchain/thornode/bifrost/pkg/chainclients/binance"
@@ -13,25 +14,32 @@ import (
 
 // LoadChains returns chain clients from chain configuration
 func LoadChains(thorKeys *thorclient.Keys, cfg []config.ChainConfiguration, server *tss.TssServer, thorchainBridge *thorclient.ThorchainBridge, m *metrics.Metrics) map[common.Chain]ChainClient {
+	logger := log.Logger.With().Str("module", "bifrost").Logger()
 	chains := make(map[common.Chain]ChainClient, 0)
 
 	for _, chain := range cfg {
 		switch chain.ChainID {
 		case common.BNBChain:
 			bnb, err := binance.NewBinance(thorKeys, chain, server, thorchainBridge, m)
-			if err == nil {
-				chains[common.BNBChain] = bnb
+			if err != nil {
+				logger.Error().Err(err).Str("chain_id", chain.ChainID.String()).Msg("fail to load chain")
+				continue
 			}
+			chains[common.BNBChain] = bnb
 		case common.ETHChain:
 			eth, err := ethereum.NewClient(thorKeys, chain, server, thorchainBridge, m)
-			if err == nil {
-				chains[common.ETHChain] = eth
+			if err != nil {
+				logger.Error().Err(err).Str("chain_id", chain.ChainID.String()).Msg("fail to load chain")
+				continue
 			}
+			chains[common.ETHChain] = eth
 		case common.BTCChain:
 			btc, err := bitcoin.NewClient(thorKeys, chain, server, thorchainBridge, m)
-			if err == nil {
-				chains[common.BTCChain] = btc
+			if err != nil {
+				logger.Error().Err(err).Str("chain_id", chain.ChainID.String()).Msg("fail to load chain")
+				continue
 			}
+			chains[common.BTCChain] = btc
 		default:
 			continue
 		}
