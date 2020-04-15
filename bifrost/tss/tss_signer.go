44597ab2048e97354266006029af5e6e3f7bf2ea
Heimdall Thor <heimdallthor02@gmail.com>
2020-04-15 17:27:58 +1000

    421-issue hash message

--- bifrost/tss/tss_signer.go
+++ bifrost/tss/tss_signer.go
@@ -1,6 +1,7 @@
 package tss
 
 import (
+	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
 	"math/big"
@@ -107,11 +108,24 @@ func (s *KeySign) SignWithPool(msg tx.StdSignMsg, poolPubKey common.PubKey, sign
 	return bz, nil
 }
 
+func (s *KeySign) hash(msg []byte) ([]byte, error) {
+	h := sha256.New()
+	_, err := h.Write(msg)
+	if err != nil {
+		return nil, fmt.Errorf("fail to calculate hash: %w", err)
+	}
+	return h.Sum(nil), nil
+}
+
 func (s *KeySign) RemoteSign(msg []byte, poolPubKey string, signerPubKeys common.PubKeys) ([]byte, error) {
 	if len(msg) == 0 {
 		return nil, nil
 	}
-	encodedMsg := base64.StdEncoding.EncodeToString(msg)
+	hashedMsg, err := s.hash(msg)
+	if err != nil {
+		return nil, fmt.Errorf("fail to calculaet msg hash: %w", err)
+	}
+	encodedMsg := base64.StdEncoding.EncodeToString(hashedMsg)
 	rResult, sResult, err := s.toLocalTSSSigner(poolPubKey, encodedMsg, signerPubKeys)
 	if err != nil {
 		return nil, fmt.Errorf("fail to tss sign: %w", err)
