31827df583214c214eb36ac14ed5d77f4031d35f
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-04-17 14:12:58 -0400

    pull docker image

--- build/docker/testnet/ec2-userdata.sh
+++ build/docker/testnet/ec2-userdata.sh
@@ -66,6 +66,7 @@ cat <<EOF > /opt/${THORNODE_ENV}/standalone-bootstrap
 
 start_stack () {
     cd $GIT_PATH/build/docker
+    docker pull registry.gitlab.com/thorchain/thornode:${THORNODE_ENV}
     export TAG=${THORNODE_ENV} && \
     export SIGNER_PASSWD=${THORNODE_PASSWD} && \
     export BINANCE_HOST="http://testnet-binance.thorchain.info:26657" && \
