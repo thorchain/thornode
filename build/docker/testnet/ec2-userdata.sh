cb7098ef030cba5bcfc571572e757fd2a1358790
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-04-19 23:49:16 -0400

    move docker enable

--- build/docker/testnet/ec2-userdata.sh
+++ build/docker/testnet/ec2-userdata.sh
@@ -24,7 +24,6 @@ apt-get install -y \
     python3-pip
 
 systemctl enable cron # enable cron
-systemctl enable docker # enable docker at boot
 
 echo "install aws cli"
 export LC_ALL=C
@@ -37,6 +36,8 @@ echo "installing docker-compose" >> $LOGFILE
 curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose version >> $LOGFILE
+
+systemctl enable docker # enable docker at boot
 
 # clone and start thornode
 echo "cloning thornode" >> $LOGFILE
