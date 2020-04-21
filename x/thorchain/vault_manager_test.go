338bfb08ad65c897c149478c776d09d731b000a1
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-04-20 22:11:56 -0400

    fix unit tests

--- x/thorchain/vault_manager_test.go
+++ x/thorchain/vault_manager_test.go
@@ -24,6 +24,7 @@ type TestRagnarokChainKeeper struct {
 	retireVault Vault
 	pools       Pools
 	ps          PoolStaker
+	sp          StakerPool
 	na          NodeAccount
 	err         error
 }
@@ -52,6 +53,15 @@ func (k *TestRagnarokChainKeeper) GetPool(_ sdk.Context, asset common.Asset) (Po
 	return Pool{}, errors.New("pool not found")
 }
 
+func (k *TestRagnarokChainKeeper) SetPool(_ sdk.Context, pool Pool) error {
+	for i, p := range k.pools {
+		if p.Asset.Equals(pool.Asset) {
+			k.pools[i] = pool
+		}
+	}
+	return k.err
+}
+
 func (k *TestRagnarokChainKeeper) PoolExist(_ sdk.Context, _ common.Asset) bool {
 	return true
 }
@@ -63,12 +73,22 @@ func (k *TestRagnarokChainKeeper) GetPoolStaker(_ sdk.Context, asset common.Asse
 	return PoolStaker{}, k.err
 }
 
-func (k *TestRagnarokChainKeeper) GetStakerPool(_ sdk.Context, addr common.Address) (StakerPool, error) {
-	if asset.Equals(common.BTCAsset) {
-		return k.ps, k.err
-	}
-	return PoolStaker{}, k.err
+func (k *TestRagnarokChainKeeper) SetPoolStaker(_ sdk.Context, ps PoolStaker) {
+	k.ps = ps
 }
+
+func (k *TestRagnarokChainKeeper) GetStakerPool(_ sdk.Context, addr common.Address) (StakerPool, error) {
+	return k.sp, k.err
+}
+
+func (k *TestRagnarokChainKeeper) SetStakerPool(_ sdk.Context, sp StakerPool) {
+	k.sp = sp
+}
+
+func (k *TestRagnarokChainKeeper) GetGas(_ sdk.Context, _ common.Asset) ([]sdk.Uint, error) {
+	return []sdk.Uint{sdk.NewUint(10)}, k.err
+}
+
 func (k *TestRagnarokChainKeeper) GetLowestActiveVersion(_ sdk.Context) semver.Version {
 	return constants.SWVersion
 }
@@ -81,8 +101,15 @@ func (k *TestRagnarokChainKeeper) UpsertEvent(_ sdk.Context, _ Event) error {
 	return k.err
 }
 
+func (k *TestRagnarokChainKeeper) IsActiveObserver(_ sdk.Context, _ sdk.AccAddress) bool {
+	return true
+}
+
 func (s *ValidatorManagerTestSuite) TestRagnarokChain(c *C) {
 	ctx, _ := setupKeeperForTest(c)
+	ctx = ctx.WithBlockHeight(100000)
+	ver := constants.SWVersion
+	constAccessor := constants.GetConstantValues(ver)
 
 	activeVault := GetRandomVault()
 	retireVault := GetRandomVault()
@@ -94,6 +121,12 @@ func (s *ValidatorManagerTestSuite) TestRagnarokChain(c *C) {
 	btcPool.BalanceAsset = sdk.NewUint(10 * common.One)
 	btcPool.PoolUnits = sdk.NewUint(1600)
 
+	bnbPool := NewPool()
+	bnbPool.Asset = common.BNBAsset
+	bnbPool.BalanceRune = sdk.NewUint(1000 * common.One)
+	bnbPool.BalanceAsset = sdk.NewUint(10 * common.One)
+	bnbPool.PoolUnits = sdk.NewUint(1600)
+
 	ps := NewPoolStaker(common.BTCAsset, sdk.NewUint(1600))
 	addr := GetRandomBNBAddress()
 	ps.Stakers = []StakerUnit{
@@ -101,22 +134,32 @@ func (s *ValidatorManagerTestSuite) TestRagnarokChain(c *C) {
 			RuneAddress: addr,
 			Height:      5,
 			Units:       ps.TotalUnits.QuoUint64(2),
+			PendingRune: sdk.ZeroUint(),
 		},
 		StakerUnit{
 			RuneAddress: GetRandomBNBAddress(),
 			Height:      10,
 			Units:       ps.TotalUnits.QuoUint64(2),
+			PendingRune: sdk.ZeroUint(),
 		},
 	}
+	sp := NewStakerPool(addr)
 
 	keeper := &TestRagnarokChainKeeper{
 		na:          GetRandomNodeAccount(NodeActive),
 		activeVault: activeVault,
 		retireVault: retireVault,
-		pools:       Pools{btcPool},
+		pools:       Pools{bnbPool, btcPool},
 		ps:          ps,
+		sp:          sp,
 	}
 
 	versionedTxOutStoreDummy := NewVersionedTxOutStoreDummy()
 	vaultMgr := NewVaultMgr(keeper, versionedTxOutStoreDummy)
+
+	err := vaultMgr.ragnarokRetiredChains(ctx, constAccessor)
+	c.Assert(err, IsNil)
+	c.Check(keeper.pools[1].Asset.Equals(common.BTCAsset), Equals, true)
+	c.Check(keeper.pools[1].PoolUnits.IsZero(), Equals, true)
+	c.Check(keeper.ps.Stakers, HasLen, 0)
 }
