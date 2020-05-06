7c6aae126b5f6477fc5f99b99e02d2339edca39b
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-05-05 23:09:28 -0400

    add dummy versionized validator mgr

--- x/thorchain/validator_manager_dummy_test.go
+++ x/thorchain/validator_manager_dummy_test.go
@@ -1,11 +1,29 @@
 package thorchain
 
 import (
+	"github.com/blang/semver"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	"gitlab.com/thorchain/thornode/constants"
 )
+
+type VersionedValidatorDummyMgr struct {
+}
+
+func NewVersionedValidatorDummyMgr() VersionedValidatorDummyMgr {
+	return VersionedValidatorDummyMgr{}
+}
+
+func (VersionedValidatorDummyMgr) BeginBlock(ctx sdk.Context, version semver.Version, constAccessor constants.ConstantValues) error {
+	return nil
+}
+func (VersionedValidatorDummyMgr) EndBlock(ctx sdk.Context, version semver.Version, constAccessor constants.ConstantValues) []abci.ValidatorUpdate {
+	return nil
+}
+func (VersionedValidatorDummyMgr) RequestYggReturn(ctx sdk.Context, version semver.Version, node NodeAccount) error {
+	return nil
+}
 
 // ValidatorDummyMgr is to manage a list of validators , and rotate them
 type ValidatorDummyMgr struct {
