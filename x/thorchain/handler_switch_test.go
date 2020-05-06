34c5105f83fd0c0eb31e19bf06716e06a1f90b0a
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-05-06 14:06:47 -0400

    [cleanup] tss chains should have rune chain

--- x/thorchain/handler_switch_test.go
+++ x/thorchain/handler_switch_test.go
@@ -23,7 +23,7 @@ func (s *HandlerSwitchSuite) TestValidate(c *C) {
 	c.Assert(k.SetNodeAccount(ctx, na), IsNil)
 	tx := GetRandomTx()
 	tx.Coins = common.Coins{
-		common.NewCoin(common.RuneAsset(), sdk.NewUint(100*common.One)),
+		common.NewCoin(common.RuneA1FAsset, sdk.NewUint(100*common.One)),
 	}
 	destination := GetRandomBNBAddress()
 
