eb1cd67bb4cc213f9f69c0d37d1a121df9fabcef
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-05-09 21:13:37 -0400

    update mock leave script

--- build/scripts/mock-leave.sh
+++ build/scripts/mock-leave.sh
@@ -16,11 +16,11 @@ fi
 
 POOL_ADDRESS=$(curl -s $1:1317/thorchain/pool_addresses | jq -r ".current[0].address")
 
-curl -vvv -s -X POST -d "{
+curl -vvv -s -X POST -d "[{
   \"from\": \"$2\",
   \"to\": \"$POOL_ADDRESS\",
   \"coins\":[
       {\"denom\": \"RUNE-A1F\", \"amount\": 100000000000000}
   ],
   \"memo\": \"LEAVE\"
-}" $1:26660/broadcast/easy
+}]" $1:26660/broadcast/easy
