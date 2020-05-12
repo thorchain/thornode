4d64f226019e546ca509039ae21fadd11fdb70cc
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-05-12 13:14:55 -0400

    fix unit tests

--- build/scripts/core.sh
+++ build/scripts/core.sh
@@ -37,6 +37,12 @@ add_gas_config () {
     done
 }
 
+reserve () {
+    jq --arg RESERVE $1 '.app_state.thorchain.reserve = $RESERVE' <~/.thord/config/genesis.json >/tmp/genesis.json
+    mv /tmp/genesis.json ~/.thord/config/genesis.json
+}
+
+
 disable_bank_send () {
     jq '.app_state.bank.send_enabled = false' <~/.thord/config/genesis.json >/tmp/genesis.json
     mv /tmp/genesis.json ~/.thord/config/genesis.json
